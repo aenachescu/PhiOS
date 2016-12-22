15f2bc6c28b04c7537cc07a4dda4a2e545343c7a
JustBeYou <mihailferaru2000@gmail.com>
2016-12-22 15:13:41 +0200

    Removed useless code.

--- kernel/src/phi_main.c
+++ kernel/src/phi_main.c
@@ -1,9 +1,6 @@
 #include <vga/text_mode.h>
 
-int drivers_test_func();
-
 void kernel_main(void) {
-    int a = drivers_test_func();
     VGA_Init();
     VGA_WriteString("Hello, PhiOS!\n");
     VGA_WriteString("Halt");
