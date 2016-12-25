3d766726e4a81274a70d600d2fab0194f72d1d8b
EnachescuAlin <alinenachescu033@gmail.com>
2016-12-25 17:23:15 +0200

    Used our typedefs

--- kernel/include/arch/x86/asm_io.h
+++ kernel/include/arch/x86/asm_io.h
@@ -1,14 +1,14 @@
 #ifndef _PhiOS_asm_io
 #define _PhiOS_asm_io
 
-#include <stdint.h>
+#include "types.h"
 
-void io_outb(uint16_t a_port, uint8_t a_val);
-void io_outw(uint16_t a_port, uint16_t a_val);
-void io_outl(uint16_t a_port, uint32_t a_val);
-uint8_t io_inb(uint16_t a_port);
-uint16_t io_inw(uint16_t a_port);
-uint32_t io_inl(uint16_t a_port);
+void io_outb(uint16 a_port, uint8 a_val);
+void io_outw(uint16 a_port, uint16 a_val);
+void io_outl(uint16 a_port, uint32 a_val);
+uint8 io_inb(uint16 a_port);
+uint16 io_inw(uint16 a_port);
+uint32 io_inl(uint16 a_port);
 void io_wait();
 
 #endif
