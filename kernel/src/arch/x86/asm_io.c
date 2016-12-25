f9cac4d04c48b165cc658e30a305e47c3623be75
EnachescuAlin <alinenachescu033@gmail.com>
2016-12-25 17:23:15 +0200

    Used our typedefs

--- kernel/src/arch/x86/asm_io.c
+++ kernel/src/arch/x86/asm_io.c
@@ -1,41 +1,41 @@
-#include <arch/x86/asm_io.h>
+#include "arch/x86/asm_io.h"
 
-void io_outb(uint16_t a_port, uint8_t a_val)
+void io_outb(uint16 a_port, uint8 a_val)
 {
     asm volatile("outb %0, %1" : : "a"(a_val), "Nd"(a_port));
 }
 
-void io_outw(uint16_t a_port, uint16_t a_val)
+void io_outw(uint16 a_port, uint16 a_val)
 {
     asm volatile("outw %0, %1" : : "a"(a_val), "Nd"(a_port));
 }
 
-void io_outl(uint16_t a_port, uint32_t a_val)
+void io_outl(uint16 a_port, uint32 a_val)
 {
     asm volatile("outl %0, %1" : : "a"(a_val), "Nd"(a_port));
 }
 
-uint8_t io_inb(uint16_t a_port)
+uint8 io_inb(uint16 a_port)
 {
-    uint8_t ret;
+    uint8 ret;
     asm volatile ( "inb %1, %0"
                    : "=a"(ret)
                    : "Nd"(a_port) );
     return ret;
 }
 
-uint16_t io_inw(uint16_t a_port)
+uint16 io_inw(uint16 a_port)
 {
-    uint16_t ret;
+    uint16 ret;
     asm volatile ( "inw %1, %0"
                    : "=a"(ret)
                    : "Nd"(a_port) );
     return ret;
 }
 
-uint32_t io_inl(uint16_t a_port)
+uint32 io_inl(uint16 a_port)
 {
-    uint32_t ret;
+    uint32 ret;
     asm volatile ( "inl %1, %0"
                    : "=a"(ret)
                    : "Nd"(a_port) );
