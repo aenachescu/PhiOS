0117bff96378c4a31dda19f6f8d97ced32e61e0d
EnachescuAlin <alinenachescu033@gmail.com>
2016-12-27 01:50:31 +0200

    Added kstrlen and kstrrev and their docs

--- util/kstdlib/src/kstring.c
+++ util/kstdlib/src/kstring.c
@@ -1,5 +1,6 @@
 #include "types.h"
 #include "kstring.h"
+#include "errors.h"
 
 void* kmemchr(const void *str, sint32 c, size_t n)
 {
@@ -61,7 +62,55 @@ char* kstrcpy(sint8 *dest, csint8 *src)
 
 }
 
-size_t kstrlen(csint8 *str)
+size_t kstrlen(const char *a_str, size_t *a_length)
 {
+    if (a_str == NULL || a_length == NULL)
+    {
+        return ERROR_NULL_POINTER;
+    }
 
+    *a_length = 0;
+    while (*a_str != '\0')
+    {
+        (*a_length)++;
+        a_str++;
+    }
+
+    return ERROR_SUCCESS;
 }
+
+size_t kstrrev(char *a_str, size_t a_length)
+{
+    if (a_str == NULL)
+    {
+        return ERROR_NULL_POINTER;
+    }
+
+    size_t length = a_length;
+    if (a_length == 0)
+    {
+        size_t error  = kstrlen(a_str, &length);
+
+        if (error != ERROR_SUCCESS)
+        {
+            return error;
+        }
+    }
+
+    char *left = a_str;
+    char *right = a_str + (length - 1);
+    char tmp;
+
+    while (left < right)
+    {
+        tmp = *left;
+        *left = *right;
+        *right = tmp;
+
+        left++;
+        right--;
+    }
+
+    return ERROR_SUCCESS;
+}
+
