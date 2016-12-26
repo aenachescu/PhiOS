00c0620a399f7d3e7c0e185746f6ce871fbd1c20
EnachescuAlin <alinenachescu033@gmail.com>
2016-12-27 01:51:33 +0200

    Created errors for internal functions

---
+++ include/errors.h
@@ -0,0 +1,11 @@
+#ifndef PhiOS_errors
+#define PhiOS_errors
+
+#define ERROR_SUCCESS               0
+#define ERROR_NULL_POINTER          1
+#define ERROR_INSUFFICIENT_BUFFER   2
+#define ERROR_UNKNOWN               3
+#define ERROR_UNSUPPORTED           4
+
+#endif
+
