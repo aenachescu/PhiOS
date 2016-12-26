7c3b9b0bfe23b4dd8215719e7678a310ee01fa00
EnachescuAlin <alinenachescu033@gmail.com>
2016-12-27 01:50:31 +0200

    Added kstrlen and kstrrev and their docs

--- util/kstdlib/include/kstring.h
+++ util/kstdlib/include/kstring.h
@@ -10,6 +10,29 @@ void* kmemmove(void *dest, const void *src, size_t n);
 void* kmemset(void *str, sint32 c, size_t n);
 int kstrcmp(csint8 *str1, csint8 *str2);
 char* kstrcpy(sint8 *dest, csint8 *src);
-size_t kstrlen(csint8 *str);
+
+/*
+ * @brief Computes the string length.
+ *
+ * @param a_str Pointer to the string for which will be computed the length.
+ * @param a_length Pointer to size_t, where will be saved the string length.
+ *
+ * @return
+ *  ERROR_SUCCESS       - if the function ends successfully
+ *  ERROR_POINTER_NULL  - if a_str is null
+ */
+size_t kstrlen(const char *a_str, size_t *a_length);
+
+/*
+ * @brief Reverse the string
+ *
+ * @param a_str Pointer to the string that must be reversed.
+ * @param a_length The string length. If is 0 then will be called strlen.
+ *
+ * @return
+ *  ERROR_SUCCESS       - if the function ends successfully
+ *  ERROR_POINTER_NULL  - if a_str is null
+ */
+size_t kstrrev(char *a_str, size_t a_length);
 
 #endif
