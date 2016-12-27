20da841684fc89bcbe4977522d240a65c8352696
EnachescuAlin <alinenachescu033@gmail.com>
2016-12-27 15:12:52 +0200

    Added ERROR_EMPTY_BUFFER and ERROR_NOT_FOUND

--- include/errors.h
+++ include/errors.h
@@ -7,6 +7,8 @@
 #define ERROR_UNKNOWN               3
 #define ERROR_UNSUPPORTED           4
 #define ERROR_SAME_POINTERS         5
+#define ERROR_EMPTY_BUFFER          6
+#define ERROR_NOT_FOUND             7
 
 #endif
 
