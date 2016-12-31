b87b4a80a3a2ccfd9abb4dd13a1aacab160099b8
JustBeYou <mihailferaru2000@gmail.com>
2016-12-31 16:13:58 +0200

    Print registers.

--- kernel/src/arch/x86/handlers.c
+++ kernel/src/arch/x86/handlers.c
@@ -6,9 +6,16 @@
 
 void standardHandler32(IntCpuState32 *a_state)
 {
-    // TODO: print all registers
+    // TODO = %d print all registers
     kprintf("--- KERNEL PANIC ---\n");
     kprintf("This interrupt is not supported.\n");
+    kprintf("cr3 = %d cr2 = %d\n");
+    kprintf("gs = %d ds = %d fs = %d es = %d ds = %d\n");
+    kprintf("edi = %d esi = %d \nebp = %d ");
+    kprintf("kesp = %d\n");
+    kprintf("edx = %d \necx = %d \nebx = %d \neax = %d\n");
+    kprintf("intNo = %d errCode = %d\n");
+    kprintf("eip = %d eflags = %d \nuesp = %d ss = %d\n");
     kprintf("--------------------\n");
     stopCpu();
 }
