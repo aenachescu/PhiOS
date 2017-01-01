678c43c00f31dd2df87a4b5c273d701a3dc53a44
Alin <alinenachescu033@gmail.com>
2017-01-01 21:53:11 +0200

    Called IDT_init32(). Removed the examples about PAA_Alloc and cpu feature

--- kernel/src/phi_main.c
+++ kernel/src/phi_main.c
@@ -4,6 +4,7 @@
 #include "kstdio.h"
 #include "arch/x86/cpuid.h"
 #include "arch/x86/gdt32.h"
+#include "arch/x86/idt32.h"
 
 #include "multiboot2.h"
 
@@ -38,39 +39,8 @@ void kernel_main(unsigned long magic, size_t addr)
     }
 
     PAA_Init();
-
-    int *p1 = 0;
-    int *p2 = 0;
-    char *c1 = 0;
-    char *c2 = 0;
-
-    PAA_Alloc(sizeof(int) * 5, (size_t*) &p1);
-    PAA_Alloc(sizeof(int) * 10, (size_t*) &p2);
-    PAA_Alloc(sizeof(char) * 5, (size_t*) &c1);
-    PAA_Alloc(sizeof(char) * 15, (size_t*) &c2);
-
-    kprintf("%u\n%u\n%u\n%u\n", p1, p2, c1, c2);
     GDT_init32();
-
-    size_t error = CPUID_Init();
-    kprintf("error: %u\n", error);
-
-    PRINT(CPU_FEATURE_FPU);
-    PRINT(CPU_FEATURE_MMX);
-    PRINT(CPU_FEATURE_SSE);
-    PRINT(CPU_FEATURE_SSE2);
-    PRINT(CPU_FEATURE_SSE3);
-    PRINT(CPU_FEATURE_SSE4_1);
-    PRINT(CPU_FEATURE_SSE4_2);
-    PRINT(CPU_FEATURE_PAE);
-    PRINT(CPU_FEATURE_PSE);
-    PRINT(CPU_FEATURE_APIC);
-    PRINT(CPU_FEATURE_SEP);
-    PRINT(CPU_FEATURE_MTRR);
-    PRINT(CPU_FEATURE_PSE_36);
-    const char *vendor = NULL;
-    CPUID_GetVendorName(&vendor);
-    kprintf("Vendor: %s\n", vendor);
+    IDT_init32();
 
     return ;
 }
