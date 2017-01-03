237dc7011b17a44d38e84096fb51565108e24851
Alin <alinenachescu033@gmail.com>
2017-01-03 23:51:29 +0200

    Added rtc

---
+++ drivers/rtc/include/rtc.h
@@ -0,0 +1,22 @@
+#ifndef PhiOS_rtc
+#define PhiOS_rtc
+
+#include "types.h"
+#include "errors.h"
+
+typedef struct rtc
+{
+    uint8 seconds;
+    uint8 minutes;
+    uint8 hours;
+    uint8 days;
+    uint8 months;
+    uint8 years;
+    uint8 centuries;
+} RTC;
+
+size_t RTC_init();
+
+size_t RTC_getDateAndTime(RTC *a_rtc);
+
+#endif
