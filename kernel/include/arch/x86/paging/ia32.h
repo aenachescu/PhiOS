770dd3c1423b864932c8457ff849c1ee21399d2f
JustBeYou <mihailferaru2000@gmail.com>
2017-01-05 16:53:18 +0200

    Add PD entry for 4MB pages. (need update)

--- kernel/include/arch/x86/paging/ia32.h
+++ kernel/include/arch/x86/paging/ia32.h
@@ -39,11 +39,28 @@ struct PageDirectory4KB_Entry
     uint32 cacheDisabled :  1;
     uint32 accessed      :  1;
     uint32 ignored1      :  1;
-    uint32 PS            :  1; // If CR4.PSE = 1, must be 0 otherwise, this entry
+    uint32 pageSize      :  1; // If CR4.PSE = 1, must be 0 otherwise, this entry
                                // maps a 4-MByte page; otherwise, ignored
     uint32 ignored2      :  4;
     uint32 address       : 20;
 };
+
+struct PageDirectory4MB_Entry
+{
+    uint32 present       :  1;
+    uint32 write         :  1;
+    uint32 user          :  1;
+    uint32 writeThrough  :  1;
+    uint32 cacheDisabled :  1;
+    uint32 accessed      :  1;
+    uint32 dirty         :  1;
+    uint32 pageSize      :  1; // Must be 1 otherwise it references a page table
+    uint32 global        :  1;
+    uint32 ignored       :  3;
+    uint32 pat           :  1;
+    uint32 unkown        :  9; // Unkown bits, must read Intel Docs
+    uint32 highAddrBits  : 10; // Bits 31:22 of physical address
+}
 
 // maps 4gb of virtual memory
 struct PageDirectory_4KB
@@ -53,7 +70,7 @@ struct PageDirectory_4KB
 
 struct PageDirectory_4MB
 {
-
+    struct PageDirectory4MB_Entry entries[PAGING_IA32_PAGE_DIRECTORY_ENTRIES_NUMBER];
 };
 
 #endif
