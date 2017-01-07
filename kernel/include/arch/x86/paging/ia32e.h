b888584b56999eb6b8fdd078768e8f8f3cc446d2
JustBeYou <mihailferaru2000@gmail.com>
2017-01-07 23:11:04 +0200

    Implement all structures.

--- kernel/include/arch/x86/paging/ia32e.h
+++ kernel/include/arch/x86/paging/ia32e.h
@@ -33,35 +33,102 @@ struct IA32E_PageMapLevel4_Entry
  */
 
 struct IA32E_PageTable_4KB_Entry
-{};
+{
+    uint32 present        :  1;
+    uint32 write          :  1;
+    uint32 user           :  1;
+    uint32 writeThrough   :  1;
+    uint32 cacheDisable   :  1;
+    uint32 accessed       :  1;
+    uint32 dirty          :  1;
+    uint32 pat            :  1;
+    uint32 global         :  1;
+    uint32 ignored1       :  3;
+    uint32 address        : 40; // M-1:12 is physical address, 51:M must be 0
+    uint32 ignored3       : 11;
+    uint32 executeDisable :  1;
+};
 
 struct IA32E_PageTable_4KB
-{};
+{
+    struct IA32E_PageTable_4KB_Entry entries[PAGING_IA32E_PTE_NUMBER];
+};
 
 struct IA32E_PageDirectory_4KB_Entry
-{};
+{
+    uint32 present        :  1;
+    uint32 write          :  1;
+    uint32 user           :  1;
+    uint32 writeThrough   :  1;
+    uint32 cacheDisable   :  1;
+    uint32 accessed       :  1;
+    uint32 ignored1       :  1;
+    uint32 pageSize       :  1;
+    uint32 ignored2       :  4;
+    uint32 address        : 40; // M-1:12 is physical address, 51:M must be 0
+    uint32 ignored3       : 11;
+    uint32 executeDisable :  1
+};
 
 struct IA32E_PageDirectory_4KB
-{};
+{
+    struct IA32E_PageDirectory_4KB_Entry entries[PAGING_IA32E_PDE_NUMBER];
+};
 
 struct IA32E_PageDirectoryPointerTable_4KB_Entry
-{};
+{
+    uint32 present        :  1;
+    uint32 write          :  1;
+    uint32 user           :  1;
+    uint32 writeThrough   :  1;
+    uint32 cacheDisable   :  1;
+    uint32 accessed       :  1;
+    uint32 ignored1       :  1;
+    uint32 pageSize       :  1;
+    uint32 ignored2       :  4;
+    uint32 address        : 40; // M-1:12 is physical address, 51:M must be 0
+    uint32 ignored3       : 11;
+    uint32 executeDisable :  1;
+};
 
 struct IA32E_PageDirectoryPointerTable_4KB
-{};
+{
+    struct IA32E_PageDirectoryPointerTable_4KB_Entry entries[PAGING_IA32E_PDPTE_NUMBER];
+};
 
 struct IA32E_PageMapLevel4_4KB
-{};
+{
+    struct IA32E_PageMapLevel4_Entry entries[PAGING_IA32E_PML4E_NUMBER];
+};
 
 /*
   * structs for PML4 with page size 2MB
   */
 
 struct IA32E_PageDirectory_2MB_Entry
-{};
+{
+    uint32 present        :  1;
+    uint32 write          :  1;
+    uint32 user           :  1;
+    uint32 writeThrough   :  1;
+    uint32 cacheDisable   :  1;
+    uint32 accessed       :  1;
+    uint32 dirty          :  1;
+    uint32 pageSize       :  1;
+    uint32 global         :  1;
+    uint32 ignored1       :  3;
+    uint32 pat            :  1;
+    uint32 alwaysZero     :  8;
+    uint32 address        : 31; // M-1:21 is physical address, 51:M must be 0
+    uint32 ignored2       :  7;
+    uint32 protectionKey  :  4;
+    uint32 executeDisable :  1;
+};
 
 struct IA32E_PageDirectory_2MB
-{};
+{
+    struct IA32E_PageDirectory_2MB_Entry entries[PAGING_IA32E_PDE_NUMBER];
+};
 
 struct IA32E_PageDirectoryPointerTable_2MB_Entry
 {
