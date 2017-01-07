9221d8cfa35b766b82bc9938e32d0c2870742d4a
Alin <alinenachescu033@gmail.com>
2017-01-07 02:07:20 +0200

    Added the structs name.

--- kernel/include/arch/x86/paging/ia32e.h
+++ kernel/include/arch/x86/paging/ia32e.h
@@ -4,4 +4,67 @@
 #include "types.h"
 #include "errors.h"
 
+/*
+ * struct PML4 entry. It's independent of page size.
+ */
+struct IA32E_PageMapLevel4_Entry
+{};
+
+/*
+ * structs for PML4 with page size 4KB
+ */
+
+struct IA32E_PageTable_4KB_Entry
+{};
+
+struct IA32E_PageTable_4KB
+{};
+
+struct IA32E_PageDirectory_4KB_Entry
+{};
+
+struct IA32E_PageDirectory_4KB
+{};
+
+struct IA32E_PageDirectoryPointerTable_4KB_Entry
+{};
+
+struct IA32E_PageDirectoryPointerTable_4KB
+{};
+
+struct IA32E_PageMapLevel4_4KB
+{};
+
+/*
+  * structs for PML4 with page size 2MB
+  */
+
+struct IA32E_PageDirectory_2MB_Entry
+{};
+
+struct IA32E_PageDirectory_2MB
+{};
+
+struct IA32E_PageDirectoryPointerTable_2MB_Entry
+{};
+
+struct IA32E_PageDirectoryPointerTable_2MB
+{};
+
+struct IA32E_PageMapLevel4_2MB
+{};
+
+/*
+ * structs for PML4 with page size 1GB
+ */
+
+struct IA32E_PageDirectoryPointerTable_1GB_Entry
+{};
+
+struct IA32E_PageDirectoryPointerTable_1GB
+{};
+
+struct IA32E_PageMapLevel4_1GB
+{};
+
 #endif
