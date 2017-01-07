db7113bbc852315b37685b45182a8bfbb935c464
Alin <alinenachescu033@gmail.com>
2017-01-07 23:57:59 +0200

    Added attribute packed. Added some comments. Fixed some errors.

--- kernel/include/arch/x86/paging/ia32e.h
+++ kernel/include/arch/x86/paging/ia32e.h
@@ -26,12 +26,13 @@ struct IA32E_PageMapLevel4_Entry
     uint32 address        : 40; // M-1:12 is physical address, 51:M must be 0
     uint32 ignored3       : 11;
     uint32 executeDisable :  1;
-};
+} __attribute__((packed));
 
 /*
  * structs for PML4 with page size 4KB
  */
 
+// maps 4KB
 struct IA32E_PageTable_4KB_Entry
 {
     uint32 present        :  1;
@@ -45,15 +46,18 @@ struct IA32E_PageTable_4KB_Entry
     uint32 global         :  1;
     uint32 ignored1       :  3;
     uint32 address        : 40; // M-1:12 is physical address, 51:M must be 0
-    uint32 ignored3       : 11;
+    uint32 ignored2       :  7;
+    uint32 protectionKey  :  4;
     uint32 executeDisable :  1;
-};
+} __attribute__((packed));
 
+// maps 2MB
 struct IA32E_PageTable_4KB
 {
     struct IA32E_PageTable_4KB_Entry entries[PAGING_IA32E_PTE_NUMBER];
-};
+} __attribute__((packed));
 
+// maps 2MB
 struct IA32E_PageDirectory_4KB_Entry
 {
     uint32 present        :  1;
@@ -63,18 +67,20 @@ struct IA32E_PageDirectory_4KB_Entry
     uint32 cacheDisable   :  1;
     uint32 accessed       :  1;
     uint32 ignored1       :  1;
-    uint32 pageSize       :  1;
+    uint32 pageSize       :  1; // must be 0
     uint32 ignored2       :  4;
     uint32 address        : 40; // M-1:12 is physical address, 51:M must be 0
     uint32 ignored3       : 11;
-    uint32 executeDisable :  1
-};
+    uint32 executeDisable :  1;
+} __attribute__((packed));
 
+// maps 1GB
 struct IA32E_PageDirectory_4KB
 {
     struct IA32E_PageDirectory_4KB_Entry entries[PAGING_IA32E_PDE_NUMBER];
-};
+} __attribute__((packed));
 
+// maps 1GB
 struct IA32E_PageDirectoryPointerTable_4KB_Entry
 {
     uint32 present        :  1;
@@ -84,27 +90,30 @@ struct IA32E_PageDirectoryPointerTable_4KB_Entry
     uint32 cacheDisable   :  1;
     uint32 accessed       :  1;
     uint32 ignored1       :  1;
-    uint32 pageSize       :  1;
+    uint32 pageSize       :  1; // must be 0
     uint32 ignored2       :  4;
     uint32 address        : 40; // M-1:12 is physical address, 51:M must be 0
     uint32 ignored3       : 11;
     uint32 executeDisable :  1;
-};
+} __attribute__((packed));
 
+// maps 512GB
 struct IA32E_PageDirectoryPointerTable_4KB
 {
     struct IA32E_PageDirectoryPointerTable_4KB_Entry entries[PAGING_IA32E_PDPTE_NUMBER];
-};
+} __attribute__((packed));
 
+// maps 256PB
 struct IA32E_PageMapLevel4_4KB
 {
     struct IA32E_PageMapLevel4_Entry entries[PAGING_IA32E_PML4E_NUMBER];
-};
+} __attribute__((packed));
 
 /*
   * structs for PML4 with page size 2MB
   */
 
+// maps 2MB
 struct IA32E_PageDirectory_2MB_Entry
 {
     uint32 present        :  1;
@@ -114,7 +123,7 @@ struct IA32E_PageDirectory_2MB_Entry
     uint32 cacheDisable   :  1;
     uint32 accessed       :  1;
     uint32 dirty          :  1;
-    uint32 pageSize       :  1;
+    uint32 pageSize       :  1; // must be 1
     uint32 global         :  1;
     uint32 ignored1       :  3;
     uint32 pat            :  1;
@@ -123,13 +132,15 @@ struct IA32E_PageDirectory_2MB_Entry
     uint32 ignored2       :  7;
     uint32 protectionKey  :  4;
     uint32 executeDisable :  1;
-};
+} __attribute__((packed));
 
+// maps 1GB
 struct IA32E_PageDirectory_2MB
 {
     struct IA32E_PageDirectory_2MB_Entry entries[PAGING_IA32E_PDE_NUMBER];
-};
+} __attribute__((packed));
 
+// maps 1GB
 struct IA32E_PageDirectoryPointerTable_2MB_Entry
 {
     uint32 present        :  1;
@@ -139,27 +150,30 @@ struct IA32E_PageDirectoryPointerTable_2MB_Entry
     uint32 cacheDisable   :  1;
     uint32 accessed       :  1;
     uint32 ignored1       :  1;
-    uint32 pageSize       :  1;
+    uint32 pageSize       :  1; // must be 0
     uint32 ignored2       :  4;
     uint32 address        : 40; // M-1:12 is physical address, 51:M must be 0
     uint32 ignored3       : 11;
     uint32 executeDisable :  1;
-};
+} __attribute__((packed));
 
+// maps 512GB
 struct IA32E_PageDirectoryPointerTable_2MB
 {
     struct IA32E_PageDirectoryPointerTable_2MB_Entry entries[PAGING_IA32E_PDPTE_NUMBER];
-};
+} __attribute__((packed));
 
+// maps 256PB
 struct IA32E_PageMapLevel4_2MB
 {
     struct IA32E_PageMapLevel4_Entry entries[PAGING_IA32E_PML4E_NUMBER];
-};
+} __attribute__((packed));
 
 /*
  * structs for PML4 with page size 1GB
  */
 
+// maps 1GB
 struct IA32E_PageDirectoryPointerTable_1GB_Entry
 {
     uint32 present        :  1;
@@ -169,7 +183,7 @@ struct IA32E_PageDirectoryPointerTable_1GB_Entry
     uint32 cacheDisable   :  1;
     uint32 accessed       :  1;
     uint32 dirty          :  1;
-    uint32 pageSize       :  1;
+    uint32 pageSize       :  1; // must be 1
     uint32 global         :  1;
     uint32 ignored1       :  3;
     uint32 pat            :  1;
@@ -178,16 +192,18 @@ struct IA32E_PageDirectoryPointerTable_1GB_Entry
     uint32 ignored2       :  7;
     uint32 protectionKey  :  4;
     uint32 executeDisable :  1;
-};
+} __attribute__((packed));
 
+// maps 512GB
 struct IA32E_PageDirectoryPointerTable_1GB
 {
     struct IA32E_PageDirectoryPointerTable_1GB_Entry entries[PAGING_IA32E_PDPTE_NUMBER];
-};
+} __attribute__((packed));
 
+// maps 256PB
 struct IA32E_PageMapLevel4_1GB
 {
     struct IA32E_PageMapLevel4_Entry entries[PAGING_IA32E_PML4E_NUMBER];
-};
+} __attribute__((packed));
 
 #endif
