c8270e33ac0ce7efef3e7b3eb4b27a9cb467364a
JustBeYou <mihailferaru2000@gmail.com>
2017-01-09 23:25:04 +0200

    Implement print CPU state function.

--- kernel/src/arch/x86/handlers32.c
+++ kernel/src/arch/x86/handlers32.c
@@ -2,22 +2,14 @@
 #include "arch/x86/handlers32.h"
 #include "kstdio.h"
 #include "cpu.h"
+#include "vga/text_mode.h"
 
 void handlers32_default(IntCpuState32 *a_state)
 {
-    /*kprintf("--- KERNEL PANIC ---\n");
-    kprintf("This interrupt is not supported.\n");
-    kprintf("cr3 = %u cr2 = %u\n", a_state->cr3, a_state->cr2);
-    kprintf("gs = %u ds = %u fs = %u es = %u ds = %u\n",
-            a_state->gs, a_state->ds, a_state->fs, a_state->es, a_state->ds);
-    kprintf("edi = %u esi = %u \nebp = %u ",
-            a_state->edi, a_state->esi, a_state->ebp);
-    kprintf("kesp = %u\n", a_state->kesp);
-    kprintf("edx = %u ecx = %u ebx = %u eax = %u\n",
-            a_state->edx, a_state->ecx, a_state->ebx, a_state->eax);
-    kprintf("intNo = %u errCode = %d\n", a_state->intNo, a_state->errCode);
-    kprintf("eip = %u eflags = %u uesp = %u ss = %u\n",
-            a_state->eip, a_state->eflags, a_state->uesp, a_state->ss);
-    kprintf("--------------------\n");*/
-    //stopCpu();
+    VGA_WriteString("[");
+    VGA_WriteColoredString("PANIC", VGA_ColorBlack, VGA_ColorRed);
+    VGA_WriteString("]");
+    VGA_WriteString(" Interrupt is not supported.\n");
+    cpu_printState(a_state);
+    stopCpu();
 }
