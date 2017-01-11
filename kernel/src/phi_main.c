2ed88bc86e96ead19aa77d903c6ed4cd753126a5
JustBeYou <mihailferaru2000@gmail.com>
2017-01-11 18:13:31 +0200

    Print info about init.

--- kernel/src/phi_main.c
+++ kernel/src/phi_main.c
@@ -8,6 +8,8 @@
 #include "arch/x86/tss32.h"
 #include "arch/x86/pit.h"
 #include "arch/x86/pic.h"
+#include "keyboard.h"
+#include "rtc.h"
 
 #include "multiboot2.h"
 
@@ -22,45 +24,62 @@ extern void jumpToUserMode();
 size_t g_userStack[2048];   // temporary user mode
 size_t g_kernelStack[2048]; // temporary kernel mode
 
+char *g_CPUvendorName;
+
 void user_main()
 {
     kprintf("Hello, world!\n");
-    while (1) {}
+    kprintf("> ");
+    while (1)
+    {
+        char c = keyboard_readKey();
+        if (c == '\n')
+        {
+            kprintf("\n> ");
+        }
+        else
+        {
+            kprintf("%c", c);
+        }
+    }
 }
 
 void kernel_main(unsigned long magic, size_t addr)
 {
+    // Inits VGA
     VGA_Init();
-    VGA_WriteString("Hello, PhiOS!\n");
-    VGA_WriteString("Halt\n");
+    kprintf("PhiOS v0.0.1 ");
 
 #if defined WORDSIZE && WORDSIZE == 32
-    VGA_WriteString("32\n");
+    VGA_WriteString("32-bit ");
 #endif
 
 #if defined WORDSIZE && WORDSIZE == 64
-    VGA_WriteString("64\n");
+    VGA_WriteString("64-bit ");
 #endif
 
     if (magic != MULTIBOOT2_BOOTLOADER_MAGIC)
     {
-        VGA_WriteString("Is not multiboot2\n");
+        VGA_WriteString("\n[PANIC] GRUB not multiboot2");
         return;
     }
 
-    VGA_WriteString("Is multiboot2\n");
+    VGA_WriteString("GRUB multiboot2\n");
 
     if (addr & 7)
     {
-        VGA_WriteString("Multiboot2 structure is not aligned");
+        VGA_WriteString("[PANIC] Multiboot2 structure is not aligned");
         return;
     }
 
-    // Inits CPUID detection
-    CPUID_Init();
-
     // Inits Placement Address Allocator
     PAA_init((size_t) &linker_kernelEnd);
+
+    // Inits CPUID detection
+    CPUID_Init();
+    PAA_alloc(256, (size_t*) g_CPUvendorName, 1);
+    CPUID_GetVendorName((const char**) &g_CPUvendorName);
+    kprintf("[CPU] %s \n", g_CPUvendorName);
 
     // Inits GDT for 32-bit
     GDT32_init();
@@ -77,6 +96,15 @@ void kernel_main(unsigned long magic, size_t addr)
 
     // Inits timer
     PIT_init((uint16) -1);
+    kprintf("[SYSTEM] Initialized timer at %d frequency.\n", OSCILLATOR_FREQUENCY);
+
+    // Inits keyboard
+    keyboard_init();
+    kprintf("[SYSTEM] Initialized keyboard.\n");
+
+    // Inits real time clock
+    RTC_init();
+    kprintf("[SYSTEM] Initialized real time clock.\n");
 
     // Go to user mode
     jumpToUserMode();
