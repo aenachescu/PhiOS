0d44a24df0dcc1459d6403090b440daddbf64c15
Alin <alinenachescu033@gmail.com>
2017-01-11 23:21:30 +0200

    Added more parameters for IA32_4KB_initKernelStruct(). Added more checks for the parameters of the IA32_4KB_initKernelStruct function.

--- kernel/src/arch/x86/paging/ia32.c
+++ kernel/src/arch/x86/paging/ia32.c
@@ -1,23 +1,38 @@
 #include "arch/x86/paging/ia32.h"
+#include "memory/pma.h"
 
 size_t IA32_4KB_initKernelStruct(struct Paging *a_paging,
-                                 size_t a_kernelStartAddr,
-                                 size_t a_kernelEndAddr)
+                                 size_t a_codeStartAddr,
+                                 size_t a_codeEndAddr,
+                                 size_t a_rodataStartAddr,
+                                 size_t a_rodataEndAddr,
+                                 size_t a_rwdataStartAddr,
+                                 size_t a_rwdataEndAddr)
 {
     if (a_paging == NULL)
     {
         return ERROR_NULL_POINTER;
     }
 
-    if (a_kernelEndAddr <= a_kernelStartAddr)
+    if (a_codeEndAddr   <= a_codeStartAddr    ||
+        a_rodataEndAddr <  a_rodataStartAddr  ||
+        a_rwdataEndAddr <  a_rwdataStartAddr)
     {
         return ERROR_INVALID_PARAMETER;
     }
 
-    if ((a_kernelStartAddr & 4095) != 0)
+    if ((a_codeStartAddr   & 4095) != 0 ||
+        (a_rodataStartAddr & 4095) != 0 ||
+        (a_rwdataStartAddr & 4095) != 0)
     {
         return ERROR_UNALIGNED_ADDRESS;
     }
 
-    return ERROR_SUCCESS;
+    size_t error = ERROR_SUCCESS;
+
+    do
+    {
+    } while (false);
+
+    return error;
 }
