2edc0ad9cfdcff6fb71d94aee0554f264d0adbe4
Alin <alinenachescu033@gmail.com>
2017-01-12 23:11:29 +0200

    Refactoring

--- kernel/src/memory/pmm.c
+++ kernel/src/memory/pmm.c
@@ -1,1 +1,21 @@
 #include "pmm.h"
+
+struct PMA *g_allocators;
+uint8 g_index;
+uint8 g_allocatorsNumber;
+
+size_t PMM_init(uint8 a_allocatorsNumber)
+{
+}
+
+size_t PMM_addAllocator(void *a_pma, uint8 a_flag)
+{
+}
+
+size_t PMA_alloc(size_t *a_address, size_t a_frames, uint8 a_flag)
+{
+}
+
+size_t PMA_free(size_t a_startAddress, size_t a_frames, uint8 a_flag)
+{
+}
