437956e071010c06549e95f6915701c776951deb
JustBeYou <mihailferaru2000@gmail.com>
2017-07-03 18:11:36 +0300

    Add bootstrap section and moved multiboot section.

--- kernel/src/arch/x86/boot.s
+++ kernel/src/arch/x86/boot.s
@@ -1,20 +1,4 @@
-# Declare constants for the multiboot header.
-.set ALIGN,    1<<0             # align loaded modules on page boundaries
-.set MEMINFO,  1<<1             # provide memory map
-.set FLAGS,    ALIGN | MEMINFO  # this is the Multiboot 'flag' field
-.set MAGIC,    0x1BADB002       # 'magic number' lets bootloader find the header
-.set CHECKSUM, -(MAGIC + FLAGS) # checksum of above, to prove we are multiboot
-
-# Declare a multiboot header that marks the program as a kernel. These are magic
-# values that are documented in the multiboot standard. The bootloader will
-# search for this signature in the first 8 KiB of the kernel file, aligned at a
-# 32-bit boundary. The signature is in its own section so the header can be
-# forced to be within the first 8 KiB of the kernel file.
-.section .multiboot
-.align 4
-.long MAGIC
-.long FLAGS
-.long CHECKSUM
+.code32
 
 # The multiboot standard does not define the value of the stack pointer register
 # (esp) and it is up to the kernel to provide a stack. This allocates room for a
@@ -26,11 +10,19 @@
 # System V ABI standard and de-facto extensions. The compiler will assume the
 # stack is properly aligned and failure to align the stack will result in
 # undefined behavior.
+
 .section .bss
 .align 16
 stack_bottom:
 .skip 16384 # 16 KiB
 stack_top:
+
+.section .text
+.global _start
+.type _start, @function
+_bootstrap:
+1:	hlt
+	jmp 1b
 
 # The linker script specifies _start as the entry point to the kernel and the
 # bootloader will jump to this position once the kernel has been loaded. It
@@ -40,17 +32,6 @@ stack_top:
 .type _start, @function
 _start:
     jmp entry
-
-    .align 8
-    multiboot_header_begin:
-        .long 0xe85250d6
-        .long 0
-        .long multiboot_header_end - multiboot_header_begin
-        .long -(0xe85250d6 + (multiboot_header_end - multiboot_header_begin))
-        .short 0
-        .short 0
-        .long 8
-    multiboot_header_end:
 
     entry:
 
