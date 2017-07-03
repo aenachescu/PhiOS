998d74f4e0cf3b3a6da338155cad7d51d9e6a423
JustBeYou <mihailferaru2000@gmail.com>
2017-07-03 18:08:48 +0300

    Add defines.

--- kernel/include/arch/x86/idt64.h
+++ kernel/include/arch/x86/idt64.h
@@ -24,11 +24,29 @@ struct IDT64
 typedef void (*ISR64_PFN)(IntCpuState64*);
 
 size_t IDT64_init();
-size_t IDT64_SetEntry(/* TODO: find sizes for args */);
-size_t IDT64_RegisterHandler(uint32 a_n, ISR64_PFN a_handler);
-extern void IDT64_Load(uint64 a_table);
+size_t IDT64_setEntry(/* TODO: find sizes for args */);
+size_t IDT64_registerHandler(uint32 a_n, ISR64_PFN a_handler);
+extern void IDT64_load(uint64 a_table);
 
 void IDT64_isrHandler(IntCpuState64 *a_state);
 void IDT64_irqHandler(IntCpuState64 *a_state);
+
+
+#define IRQ0 32 // Programmable Interrupt Timer Interrupt (handled)
+#define IRQ1 33 // Keyboard Interrupt (handled)
+#define IRQ2 34 // Cascade (used internally by the two PICs. never raised)
+#define IRQ3 35 // COM2 (if enabled)
+#define IRQ4 36 // COM1 (if enabled)
+#define IRQ5 37 // LPT2 (if enabled)
+#define IRQ6 38 // Floppy Disk (handled)
+#define IRQ7 39 // LPT1 / Unreliable "spurious" interrupt (usually)
+#define IRQ8 40 // CMOS real-time clock (if enabled)
+#define IRQ9 41 // Free for peripherals / legacy SCSI / NIC
+#define IRQ10 42 // Free for peripherals / SCSI / NIC
+#define IRQ11 43 // Free for peripherals / SCSI / NIC
+#define IRQ12 44 // PS2 Mouse
+#define IRQ13 45 // FPU / Coprocessor / Inter-processor
+#define IRQ14 46 // Primary ATA Hard Disk
+#define IRQ15 47 // Secondary ATA Hard Disk
 
 #endif
