5996b2482db417f5c23712dc66ad31b7a019039e
JustBeYou <mihailferaru2000@gmail.com>
2017-07-06 12:47:25 +0300

    Fixed multiboot 2 header and linking procedure.

--- kernel/src/arch/x86/boot64.s
+++ kernel/src/arch/x86/boot64.s
@@ -1,19 +1,35 @@
-.section .text
 .code32
 
+.section .text
+
 multiboot_header_begin:
-    .align 4
+    # 64-bit header align
+    .align 8
+
+    # Magic header value
     .long 0xe85250d6
+
+    # Architecture type: protected mode 32-bit
     .long 0
+
+    # Header size
     .long multiboot_header_end - multiboot_header_begin
+
+    # Checksum
     .long -(0xe85250d6 + (multiboot_header_end - multiboot_header_begin))
+
+    # End tag
+    .short 0
+    .short 0
+    .long 8
 multiboot_header_end:
 
 .global _start64
 .type _start64, @function
 _start64:
-    # 64-bit initialization should go here
-    # and jump...
+    # Prints "Hi" to the screen. We can't call kernel_main because we are in
+    # 32-bit mode here.
+    movl $0x07690748,0xb8000
 1:
     hlt
     jmp 1b
