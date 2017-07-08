f91d51c84f9e1ba05691779212bafd0a7698b553
Alin <alinenachescu033@gmail.com>
2017-07-08 16:17:40 +0300

    Fixed typo and compile errors. Used union in the structs of paging.

--- kernel/include/arch/x86/paging/pae.h
+++ kernel/include/arch/x86/paging/pae.h
@@ -15,21 +15,28 @@
 // maps 2MB
 struct PAE_PageDirectory_2MB_Entry
 {
-    uint64 present        :  1;
-    uint64 write          :  1;
-    uint64 user           :  1;
-    uint64 writeThrough   :  1;
-    uint64 cacheDisabled  :  1;
-    uint64 accessed       :  1;
-    uint64 dirty          :  1;
-    uint64 pageSize       :  1; // must be 1
-    uint64 global         :  1;
-    uint64 ignored        :  3;
-    uint64 pat            :  1;
-    uint64 alwaysZero1    :  8;
-    uint64 address        : 31;
-    uint64 alwaysZero2    : 11;
-    uint64 executeDisable :  1;
+    union
+    {
+        struct
+        {
+            uint64 present        :  1;
+            uint64 write          :  1;
+            uint64 user           :  1;
+            uint64 writeThrough   :  1;
+            uint64 cacheDisabled  :  1;
+            uint64 accessed       :  1;
+            uint64 dirty          :  1;
+            uint64 pageSize       :  1; // must be 1
+            uint64 global         :  1;
+            uint64 ignored        :  3;
+            uint64 pat            :  1;
+            uint64 alwaysZero1    :  8;
+            uint64 address        : 31;
+            uint64 alwaysZero2    : 11;
+            uint64 executeDisable :  1;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 // maps 1GB
@@ -45,19 +52,26 @@ struct PAE_PageDirectory_2MB
 // maps 4kb
 struct PAE_PageTable_4KB_Entry
 {
-    uint64 present        :  1;
-    uint64 write          :  1;
-    uint64 user           :  1;
-    uint64 writeThrough   :  1;
-    uint64 cacheDisabled  :  1;
-    uint64 accessed       :  1;
-    uint64 dirty          :  1;
-    uint64 pat            :  1; // reserved
-    uint64 global         :  1;
-    uint64 ignored        :  3;
-    uint64 address        : 40;
-    uint64 alwaysZero     : 11;
-    uint64 executeDisable :  1;
+    union
+    {
+        struct
+        {
+            uint64 present        :  1;
+            uint64 write          :  1;
+            uint64 user           :  1;
+            uint64 writeThrough   :  1;
+            uint64 cacheDisabled  :  1;
+            uint64 accessed       :  1;
+            uint64 dirty          :  1;
+            uint64 pat            :  1; // reserved
+            uint64 global         :  1;
+            uint64 ignored        :  3;
+            uint64 address        : 40;
+            uint64 alwaysZero     : 11;
+            uint64 executeDisable :  1;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 // maps 2mb
@@ -69,18 +83,25 @@ struct PAE_PageTable_4KB
 // maps 2mb
 struct PAE_PageDirectory_4KB_Entry
 {
-    uint64 present        :  1;
-    uint64 write          :  1;
-    uint64 user           :  1;
-    uint64 writeThrough   :  1;
-    uint64 cacheDisabled  :  1;
-    uint64 accessed       :  1;
-    uint64 ignored1       :  1;
-    uint64 pageSize       :  1; // must be 0
-    uint64 ignored2       :  4;
-    uint64 address        : 40;
-    uint64 alwaysZero     : 11;
-    uint64 executeDisable :  1;
+    union
+    {
+        struct
+        {
+            uint64 present        :  1;
+            uint64 write          :  1;
+            uint64 user           :  1;
+            uint64 writeThrough   :  1;
+            uint64 cacheDisabled  :  1;
+            uint64 accessed       :  1;
+            uint64 ignored1       :  1;
+            uint64 pageSize       :  1; // must be 0
+            uint64 ignored2       :  4;
+            uint64 address        : 40;
+            uint64 alwaysZero     : 11;
+            uint64 executeDisable :  1;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 // maps 1GB with pages of 4KB
@@ -96,14 +117,21 @@ struct PAE_PageDirectory_4KB
 // maps 1GB. It's same structure for both page sizes (4KB and 2MB).
 struct PAE_PageDirectoryPointerTable_Entry
 {
-    uint64 present       :  1;
-    uint64 alwaysZero1   :  2;
-    uint64 writeThrough  :  1;
-    uint64 cacheDisabled :  1;
-    uint64 alwaysZero2   :  4;
-    uint64 ignored       :  3;
-    uint64 address       : 40;
-    uint64 alwaysZero3   : 10;
+    union
+    {
+        struct
+        {
+            uint64 present       :  1;
+            uint64 alwaysZero1   :  2;
+            uint64 writeThrough  :  1;
+            uint64 cacheDisabled :  1;
+            uint64 alwaysZero2   :  4;
+            uint64 ignored       :  3;
+            uint64 address       : 40;
+            uint64 alwaysZero3   : 10;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 // maps 4GB with pages of 4KB
