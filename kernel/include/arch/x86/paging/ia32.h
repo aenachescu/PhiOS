e8aead06cee7d008f3c7601616b51d8d189cf5f6
Alin <alinenachescu033@gmail.com>
2017-07-08 16:17:40 +0300

    Fixed typo and compile errors. Used union in the structs of paging.

--- kernel/include/arch/x86/paging/ia32.h
+++ kernel/include/arch/x86/paging/ia32.h
@@ -15,17 +15,23 @@
 // maps 4kb of virtual memory
 struct IA32_PageTable_4KB_Entry
 {
-    uint32 present        :  1;
-    uint32 write          :  1;
-    uint32 user           :  1;
-    uint32 writeThrough   :  1;
-    uint32 cacheDisabled  :  1;
-    uint32 accessed       :  1;
-    uint32 dirty          :  1;
-    uint32 pat            :  1;
-    uint32 global         :  1;
-    uint32 ignored        :  3;
-    uint32 address        : 20;
+    union
+    {
+        struct {
+            uint32 present        :  1;
+            uint32 write          :  1;
+            uint32 user           :  1;
+            uint32 writeThrough   :  1;
+            uint32 cacheDisabled  :  1;
+            uint32 accessed       :  1;
+            uint32 dirty          :  1;
+            uint32 pat            :  1;
+            uint32 global         :  1;
+            uint32 ignored        :  3;
+            uint32 address        : 20;
+        };
+        uint32 data;
+    };
 } __attribute__((packed));
 
 // maps 4mb of virtual memory
@@ -37,17 +43,24 @@ struct IA32_PageTable_4KB
 // maps 4mb of virtual memory
 struct IA32_PageDirectory_4KB_Entry
 {
-    uint32 present       :  1;
-    uint32 write         :  1;
-    uint32 user          :  1;
-    uint32 writeThrough  :  1;
-    uint32 cacheDisabled :  1;
-    uint32 accessed      :  1;
-    uint32 ignored1      :  1;
-    uint32 pageSize      :  1; // If CR4.PSE = 1, must be 0 otherwise, this entry
-                               // maps a 4-MByte page; otherwise, ignored
-    uint32 ignored2      :  4;
-    uint32 address       : 20;
+    union
+    {
+        struct
+        {
+            uint32 present       :  1;
+            uint32 write         :  1;
+            uint32 user          :  1;
+            uint32 writeThrough  :  1;
+            uint32 cacheDisabled :  1;
+            uint32 accessed      :  1;
+            uint32 ignored1      :  1;
+            uint32 pageSize      :  1; // If CR4.PSE = 1, must be 0 otherwise, this entry
+                                       // maps a 4-MByte page; otherwise, ignored
+            uint32 ignored2      :  4;
+            uint32 address       : 20;
+        };
+        uint32 data;
+    };
 } __attribute__((packed));
 
 // maps 4gb of virtual memory
@@ -55,6 +68,10 @@ struct IA32_PageDirectory_4KB
 {
     struct IA32_PageDirectory_4KB_Entry entries[PAGING_IA32_PDE_NUMBER];
 } __attribute__((packed));
+
+/*
+ * public functions for ia32 paging with page size of 4KB
+ */
 
 size_t IA32_4KB_init(struct Paging *a_paging,
                      struct Paging *a_currentPaging);
@@ -93,19 +110,26 @@ struct IA32_4KB_Paging_FreeParam
 // maps 4MB of virtual memory
 struct IA32_PageDirectory_4MB_Entry
 {
-    uint32 present       :  1;
-    uint32 write         :  1;
-    uint32 user          :  1;
-    uint32 writeThrough  :  1;
-    uint32 cacheDisabled :  1;
-    uint32 accessed      :  1;
-    uint32 dirty         :  1;
-    uint32 pageSize      :  1; // Must be 1 otherwise it references a page table
-    uint32 global        :  1;
-    uint32 ignored       :  3;
-    uint32 pat           :  1;
-    uint32 unkown        :  9; // Unkown bits, must read Intel Docs
-    uint32 highAddrBits  : 10; // Bits 31:22 of physical address
+    union
+    {
+        struct
+        {
+            uint32 present       :  1;
+            uint32 write         :  1;
+            uint32 user          :  1;
+            uint32 writeThrough  :  1;
+            uint32 cacheDisabled :  1;
+            uint32 accessed      :  1;
+            uint32 dirty         :  1;
+            uint32 pageSize      :  1; // Must be 1 otherwise it references a page table
+            uint32 global        :  1;
+            uint32 ignored       :  3;
+            uint32 pat           :  1;
+            uint32 unkown        :  9; // Unkown bits, must read Intel Docs
+            uint32 highAddrBits  : 10; // Bits 31:22 of physical address
+        };
+        uint32 data;
+    };
 } __attribute__((packed));
 
 struct IA32_PageDirectory_4MB
--- kernel/include/arch/x86/paging/ia32e.h
+++ kernel/include/arch/x86/paging/ia32e.h
@@ -14,18 +14,25 @@
  */
 struct IA32E_PageMapLevel4_Entry
 {
-    uint64 present        :  1;
-    uint64 write          :  1;
-    uint64 user           :  1;
-    uint64 writeThrough   :  1;
-    uint64 cacheDisable   :  1;
-    uint64 accessed       :  1;
-    uint64 ignored1       :  1;
-    uint64 pageSize       :  1; // must be 0
-    uint64 ignored2       :  4;
-    uint64 address        : 40; // M-1:12 is physical address, 51:M must be 0
-    uint64 ignored3       : 11;
-    uint64 executeDisable :  1;
+    union
+    {
+        struct
+        {
+            uint64 present        :  1;
+            uint64 write          :  1;
+            uint64 user           :  1;
+            uint64 writeThrough   :  1;
+            uint64 cacheDisable   :  1;
+            uint64 accessed       :  1;
+            uint64 ignored1       :  1;
+            uint64 pageSize       :  1; // must be 0
+            uint64 ignored2       :  4;
+            uint64 address        : 40; // M-1:12 is physical address, 51:M must be 0
+            uint64 ignored3       : 11;
+            uint64 executeDisable :  1;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 /*
@@ -35,20 +42,27 @@ struct IA32E_PageMapLevel4_Entry
 // maps 4KB
 struct IA32E_PageTable_4KB_Entry
 {
-    uint64 present        :  1;
-    uint64 write          :  1;
-    uint64 user           :  1;
-    uint64 writeThrough   :  1;
-    uint64 cacheDisable   :  1;
-    uint64 accessed       :  1;
-    uint64 dirty          :  1;
-    uint64 pat            :  1;
-    uint64 global         :  1;
-    uint64 ignored1       :  3;
-    uint64 address        : 40; // M-1:12 is physical address, 51:M must be 0
-    uint64 ignored2       :  7;
-    uint64 protectionKey  :  4;
-    uint64 executeDisable :  1;
+    union
+    {
+        struct
+        {
+            uint64 present        :  1;
+            uint64 write          :  1;
+            uint64 user           :  1;
+            uint64 writeThrough   :  1;
+            uint64 cacheDisable   :  1;
+            uint64 accessed       :  1;
+            uint64 dirty          :  1;
+            uint64 pat            :  1;
+            uint64 global         :  1;
+            uint64 ignored1       :  3;
+            uint64 address        : 40; // M-1:12 is physical address, 51:M must be 0
+            uint64 ignored2       :  7;
+            uint64 protectionKey  :  4;
+            uint64 executeDisable :  1;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 // maps 2MB
@@ -60,18 +74,25 @@ struct IA32E_PageTable_4KB
 // maps 2MB
 struct IA32E_PageDirectory_4KB_Entry
 {
-    uint64 present        :  1;
-    uint64 write          :  1;
-    uint64 user           :  1;
-    uint64 writeThrough   :  1;
-    uint64 cacheDisable   :  1;
-    uint64 accessed       :  1;
-    uint64 ignored1       :  1;
-    uint64 pageSize       :  1; // must be 0
-    uint64 ignored2       :  4;
-    uint64 address        : 40; // M-1:12 is physical address, 51:M must be 0
-    uint64 ignored3       : 11;
-    uint64 executeDisable :  1;
+    union
+    {
+        struct
+        {
+            uint64 present        :  1;
+            uint64 write          :  1;
+            uint64 user           :  1;
+            uint64 writeThrough   :  1;
+            uint64 cacheDisable   :  1;
+            uint64 accessed       :  1;
+            uint64 ignored1       :  1;
+            uint64 pageSize       :  1; // must be 0
+            uint64 ignored2       :  4;
+            uint64 address        : 40; // M-1:12 is physical address, 51:M must be 0
+            uint64 ignored3       : 11;
+            uint64 executeDisable :  1;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 // maps 1GB
@@ -83,18 +104,25 @@ struct IA32E_PageDirectory_4KB
 // maps 1GB
 struct IA32E_PageDirectoryPointerTable_4KB_Entry
 {
-    uint64 present        :  1;
-    uint64 write          :  1;
-    uint64 user           :  1;
-    uint64 writeThrough   :  1;
-    uint64 cacheDisable   :  1;
-    uint64 accessed       :  1;
-    uint64 ignored1       :  1;
-    uint64 pageSize       :  1; // must be 0
-    uint64 ignored2       :  4;
-    uint64 address        : 40; // M-1:12 is physical address, 51:M must be 0
-    uint64 ignored3       : 11;
-    uint64 executeDisable :  1;
+    union
+    {
+        struct
+        {
+            uint64 present        :  1;
+            uint64 write          :  1;
+            uint64 user           :  1;
+            uint64 writeThrough   :  1;
+            uint64 cacheDisable   :  1;
+            uint64 accessed       :  1;
+            uint64 ignored1       :  1;
+            uint64 pageSize       :  1; // must be 0
+            uint64 ignored2       :  4;
+            uint64 address        : 40; // M-1:12 is physical address, 51:M must be 0
+            uint64 ignored3       : 11;
+            uint64 executeDisable :  1;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 // maps 512GB
@@ -116,22 +144,29 @@ struct IA32E_PageMapLevel4_4KB
 // maps 2MB
 struct IA32E_PageDirectory_2MB_Entry
 {
-    uint64 present        :  1;
-    uint64 write          :  1;
-    uint64 user           :  1;
-    uint64 writeThrough   :  1;
-    uint64 cacheDisable   :  1;
-    uint64 accessed       :  1;
-    uint64 dirty          :  1;
-    uint64 pageSize       :  1; // must be 1
-    uint64 global         :  1;
-    uint64 ignored1       :  3;
-    uint64 pat            :  1;
-    uint64 alwaysZero     :  8;
-    uint64 address        : 31; // M-1:21 is physical address, 51:M must be 0
-    uint64 ignored2       :  7;
-    uint64 protectionKey  :  4;
-    uint64 executeDisable :  1;
+    union
+    {
+        struct
+        {
+            uint64 present        :  1;
+            uint64 write          :  1;
+            uint64 user           :  1;
+            uint64 writeThrough   :  1;
+            uint64 cacheDisable   :  1;
+            uint64 accessed       :  1;
+            uint64 dirty          :  1;
+            uint64 pageSize       :  1; // must be 1
+            uint64 global         :  1;
+            uint64 ignored1       :  3;
+            uint64 pat            :  1;
+            uint64 alwaysZero     :  8;
+            uint64 address        : 31; // M-1:21 is physical address, 51:M must be 0
+            uint64 ignored2       :  7;
+            uint64 protectionKey  :  4;
+            uint64 executeDisable :  1;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 // maps 1GB
@@ -143,18 +178,25 @@ struct IA32E_PageDirectory_2MB
 // maps 1GB
 struct IA32E_PageDirectoryPointerTable_2MB_Entry
 {
-    uint64 present        :  1;
-    uint64 write          :  1;
-    uint64 user           :  1;
-    uint64 writeThrough   :  1;
-    uint64 cacheDisable   :  1;
-    uint64 accessed       :  1;
-    uint64 ignored1       :  1;
-    uint64 pageSize       :  1; // must be 0
-    uint64 ignored2       :  4;
-    uint64 address        : 40; // M-1:12 is physical address, 51:M must be 0
-    uint64 ignored3       : 11;
-    uint64 executeDisable :  1;
+    union
+    {
+        struct
+        {
+            uint64 present        :  1;
+            uint64 write          :  1;
+            uint64 user           :  1;
+            uint64 writeThrough   :  1;
+            uint64 cacheDisable   :  1;
+            uint64 accessed       :  1;
+            uint64 ignored1       :  1;
+            uint64 pageSize       :  1; // must be 0
+            uint64 ignored2       :  4;
+            uint64 address        : 40; // M-1:12 is physical address, 51:M must be 0
+            uint64 ignored3       : 11;
+            uint64 executeDisable :  1;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 // maps 512GB
@@ -176,22 +218,29 @@ struct IA32E_PageMapLevel4_2MB
 // maps 1GB
 struct IA32E_PageDirectoryPointerTable_1GB_Entry
 {
-    uint64 present        :  1;
-    uint64 write          :  1;
-    uint64 user           :  1;
-    uint64 writeThrough   :  1;
-    uint64 cacheDisable   :  1;
-    uint64 accessed       :  1;
-    uint64 dirty          :  1;
-    uint64 pageSize       :  1; // must be 1
-    uint64 global         :  1;
-    uint64 ignored1       :  3;
-    uint64 pat            :  1;
-    uint64 alwaysZero     : 17;
-    uint64 address        : 22; // M-1:30 must be address, 51:M must be 0
-    uint64 ignored2       :  7;
-    uint64 protectionKey  :  4;
-    uint64 executeDisable :  1;
+    union
+    {
+        struct
+        {
+            uint64 present        :  1;
+            uint64 write          :  1;
+            uint64 user           :  1;
+            uint64 writeThrough   :  1;
+            uint64 cacheDisable   :  1;
+            uint64 accessed       :  1;
+            uint64 dirty          :  1;
+            uint64 pageSize       :  1; // must be 1
+            uint64 global         :  1;
+            uint64 ignored1       :  3;
+            uint64 pat            :  1;
+            uint64 alwaysZero     : 17;
+            uint64 address        : 22; // M-1:30 must be address, 51:M must be 0
+            uint64 ignored2       :  7;
+            uint64 protectionKey  :  4;
+            uint64 executeDisable :  1;
+        };
+        uint64 data;
+    };
 } __attribute__((packed));
 
 // maps 512GB
