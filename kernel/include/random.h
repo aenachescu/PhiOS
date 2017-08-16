3e194ad3a8ceb1c6a1c674815c7c57e2350c0592
JustBeYou <mihailferaru2000@gmail.com>
2017-08-16 13:49:32 +0300

    Implement kernel random generation.

---
+++ kernel/include/random.h
@@ -0,0 +1,12 @@
+#ifndef PhiOS_Random
+#define PhiOS_Random
+
+uint16 kernel_random16();
+
+uint32 kernel_random32();
+
+uint64 kernel_random64();
+
+size_t kernel_random();
+
+#endif // PhiOS_Random
