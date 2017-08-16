77f85dc00d5af4cca2c796764c4bc15b7c0097e5
Alin <alinenachescu033@gmail.com>
2017-08-16 23:59:17 +0300

    refactoring random

--- kernel/include/arch/x86/drng.h
+++ kernel/include/arch/x86/drng.h
@@ -2,15 +2,14 @@
 #define PhiOS_DigitalRandomNumberGenerator
 
 #include "types.h"
+#include "errors.h"
 
-size_t drng_uint16(uint16 *a_value);
+uint64 drng_pseudoRandom();
 
-size_t drng_uint32(uint32 *a_value);
+uint32 drng_uint16(uint16 *a_value);
 
-#ifdef PhiOS_ARCH_x86_64
+uint32 drng_uint32(uint32 *a_value);
 
-size_t drng_uint64(uint64 *a_value);
-
-#endif // PhiOS_ARCH_x86_64
+uint32 drng_uint64(uint64 *a_value);
 
 #endif // PhiOS_DigitalRandomNumberGenerator
