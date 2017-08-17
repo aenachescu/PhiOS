e083fcae0c4678a09d89e6e6b2f1e1dfa02cd44a
JustBeYou <mihailferaru2000@gmail.com>
2017-08-17 16:52:33 +0300

    Add check function.

--- kernel/include/memory/pmm.h
+++ kernel/include/memory/pmm.h
@@ -25,6 +25,13 @@ typedef uint32 (*PMA_RESERVE_PFN)(
     uint64 physicalAddress
 );
 
+typedef uint32 (*PMA_CHECK_PFN)(
+    void *a_pma,
+    uint64 a_startAddr,
+    uint64 a_endAddr,
+    uint8 *a_state
+);
+
 struct PMA
 {
     uint8           type;
@@ -33,6 +40,7 @@ struct PMA
     PMA_ALLOC_PFN   allocFn;
     PMA_FREE_PFN    freeFn;
     PMA_RESERVE_PFN reserveFn;
+    PMA_CHECK_PFN   checkFn;
 };
 
 uint32 PMM_init(
@@ -44,7 +52,8 @@ uint32 PMM_addAllocator(
     uint8 a_flag,
     PMA_ALLOC_PFN a_allocFn,
     PMA_FREE_PFN a_freeFn,
-    PMA_RESERVE_PFN a_reserveFn
+    PMA_RESERVE_PFN a_reserveFn,
+    PMA_CHECK_PFN a_checkFn
 );
 
 uint32 PMM_alloc(
@@ -62,6 +71,13 @@ uint32 PMM_free(
 uint32 PMM_reserve(
     uint64 a_address,
     uint64 a_size,
+    uint8 a_flag
+);
+
+uint32 PMM_check(
+    uint64 a_startAddr,
+    uint64 a_endAddr,
+    uint8 *a_state,
     uint8 a_flag
 );
 
