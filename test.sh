d12daf266aa7da56865743d66e068024592fa285
Alin <alinenachescu033@gmail.com>
2017-08-19 16:26:18 +0300

    small refactor

--- test.sh
+++ test.sh
@@ -1,5 +1,70 @@
 #!/bin/bash
 
+function create_dir {
+    if ! [ -d "$1" ]; then
+        mkdir $1
+    fi
+    cd $1
+}
+
+function build_tests_x86_64 {
+    create_dir bin
+    create_dir x86_64
+
+    cmake ../.. -DARCH:STRING=x86_64
+    rc=$?
+    if [[ $rc != 0 ]]; then
+        echo "${red}cmake failed${reset} - $rc"
+        exit $rc
+    fi
+
+    make
+    rc=$?
+    if [[ $rc != 0 ]]; then
+        echo "${red}make failed${reset} - $rc"
+        exit $rc
+    fi
+}
+
+function build_tests_x86_32 {
+    create_dir bin
+    create_dir x86_32
+
+    cmake ../.. -DARCH:STRING=x86_32
+    rc=$?
+    if [[ $rc != 0 ]]; then
+        echo "${red}cmake failed${reset} - $rc"
+        exit $rc
+    fi
+
+    make
+    rc=$?
+    if [[ $rc != 0 ]]; then
+        echo "${red}make failed${reset} - $rc"
+        exit $rc
+    fi   
+}
+
+function running_tests {
+    # running all tests
+    echo -e "\n${green}*****${reset} Running tests"
+    rc=0
+
+    # running PAA tests
+    echo -e "${blue}***${reset} Running test_PAA"
+    ./test_paa
+    rc=$((rc + $?))
+
+    # running BitmapPMA tests
+    echo -e "\n${blue}***${reset} Running test_BitmapPMA"
+    ./test_bitmap
+    rc=$((rc + $?))
+
+    exit $rc
+}
+
+# define colors
+red=`tput setaf 1`
 green=`tput setaf 2`
 blue=`tput setaf 4`
 reset=`tput sgr0`
@@ -17,30 +82,30 @@ export PATH=$PWD/build/linux-x86_64/cmake/bin:$PATH
 # Change to tests directory
 cd tests
 
-# building tests
-echo -e "\n${green}*****${reset} Building tests"
-cmake .
-make clean
-make
-
-err=$?
-if [[ $err != 0 ]]; then
-    echo "make failed - $err"
-    exit $err
+if [ $# -ne 2 ]; then
+    echo "${red}Illegal number of parameters${reset}"
+    exit 1
 fi
 
-# running all tests
-echo -e "\n${green}*****${reset} Running tests"
-rc=0
+echo -e "\n${green}*****${reset} Building tests"
 
-# running PAA tests
-echo -e "${blue}***${reset} Running test_PAA"
-./test_paa
-rc=$((rc + $?))
+if [ "$1" == "run" ];then
 
-# running BitmapPMA tests
-echo -e "\n${blue}***${reset} Running test_BitmapPMA"
-./test_bitmap
-rc=$((rc + $?))
+    if [ "$2" == "x86_64" ]; then
+        build_tests_x86_64
+        echo "${green}build tests for x86_64 successfully${reset}"
+        running_tests
+    fi
 
-exit $rc
+    if [ "$2" == "x86_32" ]; then
+        build_tests_x86_32
+        echo "${green}build tests for x86_32 successfully${reset}"
+        running_tests
+    fi
+
+    echo "${red}invalid parameter for run command${reset}"
+    exit 1
+fi
+
+echo "${red}invalid syntax${reset}"
+exit 1
