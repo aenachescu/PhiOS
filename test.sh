7c824d73c6cbc162b6937434b7d5157522afac3e
Alin <alinenachescu033@gmail.com>
2017-08-19 16:44:14 +0300

    fixed some minor bugs

--- test.sh
+++ test.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# define colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+blue=`tput setaf 4`
+reset=`tput sgr0`
+
 function create_dir {
     if ! [ -d "$1" ]; then
         mkdir $1
     fi
     cd $1
+}
+
+function remove_dir {
+    if [ -d "$1" ]; then
+        rm -rf $1
+    fi
 }
 
 function build_tests_x86_64 {
@@ -63,47 +75,67 @@ function running_tests {
     exit $rc
 }
 
-# define colors
-red=`tput setaf 1`
-green=`tput setaf 2`
-blue=`tput setaf 4`
-reset=`tput sgr0`
+function configEnvironment {
+    # building CUT library
+    echo -e "${green}*****${reset} Building CUT"
+    cd CUT/src
+    make clean
+    make
+    cd ../..
 
-# building CUT library
-echo -e "${green}*****${reset} Building CUT"
-cd CUT/src
-make clean
-make
-cd ../..
+    # set path to cmake
+    export PATH=$PWD/build/linux-x86_64/cmake/bin:$PATH
 
-# set path to cmake
-export PATH=$PWD/build/linux-x86_64/cmake/bin:$PATH
-
-# Change to tests directory
-cd tests
+    # Change to tests directory
+    cd tests
+}
 
 if [ $# -ne 2 ]; then
     echo "${red}Illegal number of parameters${reset}"
     exit 1
 fi
 
-echo -e "\n${green}*****${reset} Building tests"
-
 if [ "$1" == "run" ];then
-
     if [ "$2" == "x86_64" ]; then
+        configEnvironment
+
+        echo -e "\n${green}*****${reset} Building tests for x86_64"
         build_tests_x86_64
-        echo "${green}build tests for x86_64 successfully${reset}"
+        echo "${green}builded tests for x86_64 successfully${reset}"
+
         running_tests
     fi
 
     if [ "$2" == "x86_32" ]; then
+        configEnvironment
+
+        echo -e "\n${green}*****${reset} Building tests for x86_32"
         build_tests_x86_32
-        echo "${green}build tests for x86_32 successfully${reset}"
+        echo "${green}builded tests for x86_32 successfully${reset}"
+
         running_tests
     fi
 
     echo "${red}invalid parameter for run command${reset}"
+    exit 1
+fi
+
+if [ "$1" == "clean" ];then
+    cd tests
+
+    if [ "$2" == "x86_32" ]; then
+        remove_dir bin/x86_32
+        echo "${green}clean x86_32 successfully${reset}"
+        exit 0
+    fi
+
+    if [ "$2" == "x86_64" ]; then
+        remove_dir bin/x86_64
+        echo "${green}clean x86_64 successfully${reset}"
+        exit 0
+    fi
+
+    echo "${red}invalid parameter for clean command${reset}"
     exit 1
 fi
 
