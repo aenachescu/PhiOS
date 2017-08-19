3b47711ad51c42987eda758e178d31af8d8d2e9c
Alin <alinenachescu033@gmail.com>
2017-08-19 15:47:29 +0300

    fixed build

--- test.sh
+++ test.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 
-# Compile and install CUT
+green=`tput setaf 2`
+blue=`tput setaf 4`
+reset=`tput sgr0`
+
+# building CUT library
+echo -e "${green}*****${reset} Building CUT"
 cd CUT/src
 make clean
 make
-cd ..
-sudo ./install.sh
-cd ..
+cd ../..
+
+# set path to cmake
+export PATH=$PWD/build/linux-x86_64/cmake/bin:$PATH
 
 # Change to tests directory
 cd tests
 
-# Configure and compile
+# building tests
+echo -e "\n${green}*****${reset} Building tests"
 cmake .
 make clean
 make
 
-# Run and return
+err=$?
+if [[ $err != 0 ]]; then
+    echo "make failed - $err"
+    exit $err
+fi
+
+# running all tests
+echo -e "\n${green}*****${reset} Running tests"
 rc=0
+
+# running PAA tests
+echo -e "${blue}***${reset} Running test_PAA"
 ./test_paa
 rc=$((rc + $?))
+
+# running BitmapPMA tests
+echo -e "\n${blue}***${reset} Running test_BitmapPMA"
 ./test_bitmap
 rc=$((rc + $?))
+
 exit $rc
