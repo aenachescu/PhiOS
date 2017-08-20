7ab1322535089973db5ce16dd8c291be542cf2b3
Alin <alinenachescu033@gmail.com>
2017-08-20 09:44:44 +0300

    another refactor for testing

--- tests/src/test_bitmap.c
+++ tests/src/test_bitmap.c
@@ -1,5 +1,47 @@
 #include <cut.h>
-#include "bitmap_pma.h"
+
+#include <stdlib.h>
+
+#include "kernel/include/memory/bitmap_pma.h"
+#include "kernel/include/memory/paa.h"
+
+void *paa_memory;
+void *bpma_memory;
+
+CUT_DEFINE_TEST(test_bitmapCreate)
+{
+    uint32 err;
+    paa_memory = malloc(0x1000 * 25);
+    bpma_memory = malloc(0x1000 * 50);
+
+    struct BitmapPMA bpma;
+    err = BitmapPMA_createAllocator(&bpma, 
+                                    0x1000, 
+                                    (uint64) bpma_memory, 
+                                    (uint64) ((uint64) bpma_memory + 0x1000 * 50));
+    CUT_CHECK(err == ERROR_SUCCESS);
+}
+
+CUT_DEFINE_TEST(test_bitmapAlloc)
+{
+
+}
+
+CUT_DEFINE_TEST(test_bitmapFree)
+{
+
+}
+
+CUT_DEFINE_TEST(test_bitmapReserve)
+{
+
+}
+
+CUT_DEFINE_TEST(test_bitmapCheck)
+{
+    free(paa_memory);
+    free(bpma_memory);
+}
 
 CUT_DEFINE_MAIN
     CUT_CALL_TEST(test_bitmapCreate);
