245f1ac45096b1da9bb4124886c93702573df2fc
Alin <alinenachescu033@gmail.com>
2017-09-06 14:50:18 +0300

    refactoring logging; implemented %d

--- kernel/src/logging.c
+++ kernel/src/logging.c
@@ -1,153 +1,520 @@
-#include "include/logging.h"
+#include "kernel/include/logging.h"
+#include "include/types.h"
 
-uint32 __klog(const char *a_format, ...)
+#include <stdarg.h>
+
+#include "drivers/video/include/vga/text_mode.h"
+
+#include "util/kstdlib/include/kstdlib.h"
+
+#include "include/cpu.h"
+#include "drivers/serial/include/serial.h"
+
+#define BUFFER_SIZE 512
+#define TMP_BUFF_LENGTH 65
+
+#define LOG_TYPE_MAX_LENGTH 8
+#define LOG_LINE_MAX_LENGTH 6
+
+#define LENGTH_none 0
+#define LENGTH_hh   1
+#define LENGTH_h    2
+#define LENGTH_l    3
+#define LENGTH_ll   4
+#define LENGTH_j    5
+#define LENGTH_z    6
+#define LENGTH_t    7
+#define LENGTH_L    8
+
+#define IS_DIGIT(x) (x >= '0' && x <= '9')
+
+#define DEFAULT_CASES       \
+    case '%':               \
+        goto reinit;        \
+    case '\0':              \
+        goto end;           \
+    default:                \
+        exit = true;
+
+#define REINIT              \
+    backupFormat = NULL;    \
+    ++a_format;             \
+    goto reinit
+
+static inline void write(
+    const char *str)
 {
-    if (a_format == NULL) {
-        return ERROR_NULL_POINTER;
+    VGA_WriteString(str);
+    serial_writeString(str, SERIAL_PORT_A);
+}
+
+static inline void addChar(
+    char *res,
+    char c,
+    uint32 *pos)
+{
+    if (*pos == BUFFER_SIZE - 1) {
+        res[*pos] = 0;
+        write(res);
+        *pos = 0;
+    }
+    res[*pos] = c;
+    (*pos)++;
+}
+
+static inline void addChars(
+    char *res,
+    char c,
+    uint32 num,
+    uint32 *pos)
+{
+    while (true) {
+        while (*pos < BUFFER_SIZE - 1 && num > 0) {
+            res[*pos] = c;
+            --num;
+            ++(*pos);
+        }
+
+        if (num == 0) {
+            return;
+        }
+
+        res[*pos] = 0;
+        write(res);
+        *pos = 0;
+    }
+}
+
+static inline void addString(
+    char *res,
+    const char *str,
+    uint32 *pos)
+{
+    while (true) {
+        while (*pos < BUFFER_SIZE - 1 && *str != '\0') {
+            res[*pos] = *str;
+            str++;
+            (*pos)++;
+        }
+
+        if (*str == '\0') {
+            return;
+        }
+
+        res[*pos] = 0;
+        write(res);
+        *pos = 0;
+    }
+}
+
+static inline void intToStr(
+    sint32 val,
+    char *str,
+    size_t *length,
+    size_t base,
+    bool flag_space,
+    bool flag_plus)
+{
+    if ((flag_space == false && flag_plus == false) || val < 0) {
+        kitoa(val, str, length, base);
+        return;
     }
 
+    if (flag_space == true) {
+        str[0] = ' ';
+    } else if (flag_plus == true) {
+        str[0] = '+';
+    }
+
+    --(*length);
+    kitoa(val, str + 1, length, base);
+    ++(*length);
+}
+
+static inline void int64ToStr(
+    sint64 val,
+    char *str,
+    size_t *length,
+    size_t base,
+    bool flag_space,
+    bool flag_plus)
+{
+    if ((flag_space == false && flag_plus == false) || val < 0) {
+        ki64toa(val, str, length, base);
+        return;
+    }
+
+    if (flag_space == true) {
+        str[0] = ' ';
+    } else if (flag_plus == true) {
+        str[0] = '+';
+    }
+
+    --(*length);
+    kitoa(val, str + 1, length, base);
+    ++(*length);
+}
+
+void __klog(
+    const char *a_file,
+    int a_line,
+    const char *a_logType,
+    const char *a_format,
+    ...)
+{
+    if (a_format == NULL ||
+        a_file == NULL ||
+        a_logType == NULL ||
+        a_line < 0) {
+        return;
+    }
+
+    char result[BUFFER_SIZE] = { 0 };
+    char tmpBuff[TMP_BUFF_LENGTH] = { 0 };
+    size_t tmpBuffLength = TMP_BUFF_LENGTH;
+    uint32 currentPos = 0;
     char c;
-    const char *str = NULL;
-    size_t address;
-    sint32 svalue;
-    uint32 uvalue;
+    const char *backupFormat = NULL;
+    bool exit;
 
-    char buffer[32] = { 0 };
-    size_t bufferSize = 32;
+    bool flag_minus, flag_plus, flag_space, flag_zero, flag_hash;
+    bool processingFormat;
+    uint32 width, precision, length;
+    bool widthAsterix, precisionAsterix;
 
-    bool extend = false;
-    char ebuffer[64] = { 0 };
-    uint64 eaddress;
-    sint64 esvalue;
-    uint64 euvalue;
-
-    char tmp[3] = { 0 };
-    tmp[0] = '%';
-
+    uint32 uval32;
+    uint64 uval64;
+    sint32 val32;
+    sint64 val64;
 
     va_list arg;
     va_start(arg, a_format);
 
+reinit:
+    processingFormat = false;
+    widthAsterix = false;
+    precisionAsterix = false;
+    flag_minus = false;
+    flag_plus = false;
+    flag_space = false;
+    flag_zero = false;
+    flag_hash = false;
+    width = 0;
+    precision = 0;
+    length = LENGTH_none;
+    tmpBuffLength = TMP_BUFF_LENGTH;
+
+    if (backupFormat != NULL) {
+        while (backupFormat <= a_format) {
+            addChar(result, *backupFormat, &currentPos);
+            backupFormat++;
+        }
+
+        backupFormat = NULL;
+        a_format++;
+    }
+
     while ((c = *a_format) != '\0') {
+        // backup for pointer if the format does not meet the standard
+        backupFormat = a_format;
         ++a_format;
 
-        if (c == '%' || extend) {
-            switch (*a_format) {
-                case 'd':
-                    if (extend) {
-                        esvalue = va_arg(arg, sint64);
-
-                        bufferSize = 64;
-                        ki64toa(esvalue, ebuffer, &bufferSize, 10);
-
-                        VGA_WriteString(ebuffer);
-                    } else {
-                        svalue = va_arg(arg, sint32);
-
-                        bufferSize = 32;
-                        kitoa(svalue, buffer, &bufferSize, 10);
-
-                        VGA_WriteString(buffer);
-                    }
-                    extend = false;
-                    break;
-
-                case 'u':
-                    if (extend) {
-                        euvalue = va_arg(arg, uint64);
-
-                        bufferSize = 64;
-                        ku64toa(euvalue, ebuffer, &bufferSize, 10);
-
-                        VGA_WriteString(ebuffer);
-                    } else {
-                        uvalue = va_arg(arg, uint32);
-
-                        bufferSize = 32;
-                        kutoa(uvalue, buffer, &bufferSize, 10);
-
-                        VGA_WriteString(buffer);
-                    }
-                    extend = false;
-                    break;
-
-                case 's':
-                    str = va_arg(arg, const char*);
-
-                    if (str == NULL) {
-                        VGA_WriteString("(null)");
-                    } else {
-                        VGA_WriteString(str);
-                    }
-                    extend = false;
-                    break;
-
-                case 'c':
-                    c = va_arg(arg, int);
-
-                    VGA_WriteChar(c);
-                    extend = false;
-                    break;
-
-                case 'p':
-                    address = va_arg(arg, uint32);
-
-                    bufferSize = 32;
-                    kutoa(address, buffer, &bufferSize, 16);
-
-                    VGA_WriteChar('0');
-                    VGA_WriteChar('x');
-                    VGA_WriteString(buffer);
-                    extend = false;
-                    break;
-
-                case 'x':
-                    if (extend) {
-                        eaddress = va_arg(arg, uint64);
-
-                        bufferSize = 64;
-                        ku64toa(eaddress, ebuffer, &bufferSize, 16);
-
-                        VGA_WriteChar('0');
-                        VGA_WriteChar('x');
-
-                        for (uint32 i = 64 / 4 - bufferSize; i > 0; i--)
-                        VGA_WriteChar('0');
-                        VGA_WriteString(ebuffer);
-                    } else {
-                        address = va_arg(arg, uint32);
-
-                        bufferSize = 32;
-                        kutoa(address, buffer, &bufferSize, 16);
-
-                        VGA_WriteChar('0');
-                        VGA_WriteChar('x');
-
-                        for (uint32 i = WORDSIZE / 4 - bufferSize; i > 0; i--) {
-                            VGA_WriteChar('0');
-                        }
-                        VGA_WriteString(buffer);
-                    }
-                    extend = false;
-                    break;
-
-                case 'l':
-                    if (*(a_format + 1) == 'l')  {
-                        extend = true;
-                    }
-                    break;
-
-                default:
-                    tmp[1] = *a_format;
-                    VGA_WriteString(tmp);
-            }
-            ++a_format;
-        } else {
-            VGA_WriteChar(c);
+        if (c != '%') {
+            addChar(result, c, &currentPos);
+            continue;
         }
+
+        // for %% case
+        if (*a_format == '%') {
+            addChar(result, c, &currentPos);
+            ++a_format;
+            continue;
+        }
+
+        processingFormat = true;
+
+        // processing flags
+        exit = false;
+        while (true) {
+            switch (*a_format) {
+                case '-':
+                    flag_minus = true;
+                    break;
+
+                case '+':
+                    flag_plus = true;
+                    break;
+
+                case ' ':
+                    flag_space = true;
+                    break;
+
+                case '#':
+                    flag_hash = true;
+                    break;
+
+                case '0':
+                    flag_zero = true;
+                    break;
+
+                DEFAULT_CASES;
+            }
+
+            if (exit == true) {
+                break;
+            }
+
+            ++a_format;
+        }
+
+        // processing width
+        exit = false;
+        while (true) {
+            switch (*a_format) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    width *= 10;
+                    width += (*a_format - '0');
+                    break;
+
+                case '*':
+                    widthAsterix = true;
+                    exit = true;
+                    break;
+
+                DEFAULT_CASES;
+            }
+
+            if (exit == true) {
+                break;
+            }
+
+            ++a_format;
+        }
+
+        // processing precision
+        if (*a_format != '.') {
+            goto skipPrecision;
+        }
+        ++a_format;
+        exit = false;
+        while (true) {
+            switch (*a_format) {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    precision *= 10;
+                    precision += (*a_format - '0');
+                    break;
+
+                case '*':
+                    precisionAsterix = true;
+                    exit = true;
+                    break;
+
+                DEFAULT_CASES;
+            }
+
+            if (exit == true) {
+                break;
+            }
+
+            ++a_format;
+        }
+skipPrecision:
+
+        // processing length
+        exit = false;
+        switch (*a_format) {
+            case 'h':
+                if (a_format[1] == 'h') {
+                    length = LENGTH_hh;
+                } else {
+                    length = LENGTH_h;
+                }
+                break;
+
+            case 'l':
+                if (a_format[1] == 'l') {
+                    length = LENGTH_ll;
+                } else {
+                    length = LENGTH_l;
+                }
+                break;
+
+            case 'j':
+                length = LENGTH_j;
+                break;
+
+            case 'z':
+                length = LENGTH_z;
+                break;
+
+            case 't':
+                length = LENGTH_t;
+                break;
+
+            case 'L':
+                length = LENGTH_L;
+                break;
+
+            DEFAULT_CASES;
+        }
+
+        if (exit == false) {
+            ++a_format;
+        }
+
+        // processing specifiers
+        exit = false;
+        switch (*a_format) {
+            case 'd':
+            case 'i':
+                if (widthAsterix == true) {
+                    width = va_arg(arg, uint32);
+                }
+
+                switch (length) {
+                    case LENGTH_L:
+                        // interpreted as int32
+                    case LENGTH_l:
+                    case LENGTH_none:
+                        // int32
+                        val32 = va_arg(arg, sint32);
+                        intToStr(val32, tmpBuff, &tmpBuffLength, 10, flag_space, flag_plus);
+                        break;
+
+                    case LENGTH_j:
+                        // intmax_t - interpreted as int64
+                    case LENGTH_ll:
+                        // int64
+                        val64 = va_arg(arg, sint64);
+                        int64ToStr(val64, tmpBuff, &tmpBuffLength, 10, flag_space, flag_plus);
+                        break;
+
+                    case LENGTH_h:
+                        // int16
+                        val32 = (sint32) va_arg(arg, sint32);
+                        intToStr(val32, tmpBuff, &tmpBuffLength, 10, flag_space, flag_plus);
+                        break;
+
+                    case LENGTH_hh:
+                        // int8
+                        val32 = (sint32) va_arg(arg, sint32);
+                        intToStr(val32, tmpBuff, &tmpBuffLength, 10, flag_space, flag_plus);
+                        break;
+
+                    case LENGTH_t:
+                        // ptrdiff_t - interpreted as int32 on 32bits and int64 on 64bits
+                    case LENGTH_z:
+                        // size_t
+#if defined WORDSIZE && WORDSIZE == 64
+                        val64 = va_arg(arg, sint64);
+                        int64ToStr(val64, tmpBuff, &tmpBuffLength, 10, flag_space, flag_plus);
+#else
+                        val32 = va_arg(arg, sint32);
+                        intToStr(val32, tmpBuff, &tmpBuffLength, 10, flag_space, flag_plus);
+#endif
+                        break;
+                }
+
+                if (tmpBuffLength < width) {
+                    if (flag_minus == true) {
+                        addString(result, tmpBuff, &currentPos);
+                        addChars(result, ' ', width - tmpBuffLength, &currentPos);
+                    } else {
+                        if (flag_zero == true) {
+                            if (IS_DIGIT(tmpBuff[0])) {
+                                addChars(result, '0', width - tmpBuffLength, &currentPos);
+                                addString(result, tmpBuff, &currentPos);
+                            } else {
+                                addChar(result, tmpBuff[0], &currentPos);
+                                addChars(result, '0', width - tmpBuffLength, &currentPos);
+                                addString(result, tmpBuff + 1, &currentPos);
+                            }
+                        } else {
+                            addChars(result, ' ', width - tmpBuffLength, &currentPos);
+                            addString(result, tmpBuff, &currentPos);
+                        }
+                    }
+                } else {
+                    addString(result, tmpBuff, &currentPos);
+                }
+
+                REINIT;
+
+            case 'u':
+                REINIT;
+
+            case 'o':
+                REINIT;
+
+            case 'x':
+            case 'X':
+                REINIT;
+
+            case 'f':
+            case 'F':
+                REINIT;
+
+            case 'e':
+            case 'E':
+                REINIT;
+
+            case 'g':
+            case 'G':
+                REINIT;
+
+            case 'a':
+            case 'A':
+                REINIT;
+
+            case 'p':
+            case 'P':
+                REINIT;
+
+            case 'c':
+                REINIT;
+
+            case 's':
+                REINIT;
+
+            case 'n':
+                // interpreted as invalid...for now...
+                goto reinit;
+
+            DEFAULT_CASES;
+        }
+
+        goto reinit;
     }
 
     va_end(arg);
 
-    return ERROR_SUCCESS;
+end:
+    if (processingFormat == true && backupFormat != NULL) {
+        addString(result, "ana are mere", &currentPos);
+        while (backupFormat <= a_format) {
+            addChar(result, *backupFormat, &currentPos);
+            backupFormat++;
+        }
+    }
+
+    result[currentPos] = 0;
+    write(result);
 }
