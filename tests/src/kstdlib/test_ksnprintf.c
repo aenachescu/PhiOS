fea0aca4ee24f928bea979aa6eb3967a64a0e4c3
Alin <alinenachescu033@gmail.com>
2017-09-14 23:41:34 +0300

    added tests for ksnprintf, kvsnprintf and ksnprintf_helper in same file

--- tests/src/kstdlib/test_ksnprintf.c
+++ tests/src/kstdlib/test_ksnprintf.c
@@ -2,11 +2,21 @@
 
 #include <cut.h>
 
+#include "util/kstdlib/include/ksnprintf_helper.h"
+#include "util/kstdlib/include/ksnprintf.h"
+#include "util/kstdlib/include/kvsnprintf.h"
+
 CUT_DEFINE_TEST(test_ksnprintf)
+{
+
+}
+
+CUT_DEFINE_TEST(test_kvsnprintf)
 {
 
 }
 
 CUT_DEFINE_MODULE(module_ksnprintf)
     CUT_CALL_TEST(test_ksnprintf);
+    CUT_CALL_TEST(test_kvsnprintf);
 CUT_END_MODULE
