a263ceddde9a2ed80a675803df99dd6429d41441
Alin <alinenachescu033@gmail.com>
2017-09-22 22:03:04 +0300

    changed naming

--- kernel/include/logging.h
+++ kernel/include/logging.h
@@ -9,66 +9,62 @@ void __klog(
     ...
 );
 
-#define DEFAULT_FORMAT  "[%#10s][%5d][%7s]: "
-#define NEW_LINE        "\n"
+#define PhiOS_LOGGING_DEFAULT_FORMAT  "[%#10s][%5d][%7s]: "
+#define PhiOS_LOGGING_NEW_LINE        "\n"
 
-#define KLOG_FATAL(format, ...)         \
-    __klog(                             \
-        DEFAULT_FORMAT format NEW_LINE, \
-        __FILENAME__,                   \
-        __LINE__,                       \
-        "FATAL",                        \
-        ##__VA_ARGS__                   \
+#define KLOG_FATAL(format, ...)                                     \
+    __klog(                                                         \
+        PhiOS_LOGGING_DEFAULT_FORMAT format PhiOS_LOGGING_NEW_LINE, \
+        __FILENAME__,                                               \
+        __LINE__,                                                   \
+        "FATAL",                                                    \
+        ##__VA_ARGS__                                               \
     )
 
-#define KLOG_ERROR(format, ...)         \
-    __klog(                             \
-        DEFAULT_FORMAT format NEW_LINE, \
-        __FILENAME__,                   \
-        __LINE__,                       \
-        "ERROR",                        \
-        ##__VA_ARGS__                   \
+#define KLOG_ERROR(format, ...)                                     \
+    __klog(                                                         \
+        PhiOS_LOGGING_DEFAULT_FORMAT format PhiOS_LOGGING_NEW_LINE, \
+        __FILENAME__,                                               \
+        __LINE__,                                                   \
+        "ERROR",                                                    \
+        ##__VA_ARGS__                                               \
     )
 
-#define KLOG_WARNING(format, ...)       \
-    __klog(                             \
-        DEFAULT_FORMAT format NEW_LINE, \
-        __FILENAME__,                   \
-        __LINE__,                       \
-        "WARNING",                      \
-        ##__VA_ARGS__                   \
+#define KLOG_WARNING(format, ...)                                   \
+    __klog(                                                         \
+        PhiOS_LOGGING_DEFAULT_FORMAT format PhiOS_LOGGING_NEW_LINE, \
+        __FILENAME__,                                               \
+        __LINE__,                                                   \
+        "WARNING",                                                  \
+        ##__VA_ARGS__                                               \
     )
 
-#define KLOG_INFO(format, ...)          \
-    __klog(                             \
-        DEFAULT_FORMAT format NEW_LINE, \
-        __FILENAME__,                   \
-        __LINE__,                       \
-        "INFO",                         \
-        ##__VA_ARGS__                   \
+#define KLOG_INFO(format, ...)                                      \
+    __klog(                                                         \
+        PhiOS_LOGGING_DEFAULT_FORMAT format PhiOS_LOGGING_NEW_LINE, \
+        __FILENAME__,                                               \
+        __LINE__,                                                   \
+        "INFO",                                                     \
+        ##__VA_ARGS__                                               \
     )
 
-#define KLOG_TRACE(format, ...)         \
-    __klog(                             \
-        DEFAULT_FORMAT format NEW_LINE, \
-        __FILENAME__,                   \
-        __LINE__,                       \
-        "TRACE",                        \
-        ##__VA_ARGS__                   \
+#define KLOG_TRACE(format, ...)                                     \
+    __klog(                                                         \
+        PhiOS_LOGGING_DEFAULT_FORMAT format PhiOS_LOGGING_NEW_LINE, \
+        __FILENAME__,                                               \
+        __LINE__,                                                   \
+        "TRACE",                                                    \
+        ##__VA_ARGS__                                               \
     )
 
-typedef uint32 (*LOGGING_WRITE_PFN)(const char *str);
-
-#ifndef LOGGING_MAX_PFN
-#define LOGGING_MAX_PFN 5
-#endif
+typedef uint32 (*logging_writePfn)(const char *str);
 
 uint32 logging_init();
 uint32 logging_uninit();
 bool logging_isInitialized();
 
 uint32 logging_addPfn(
-    LOGGING_WRITE_PFN a_pfn
+    logging_writePfn a_pfn
 );
 
 #endif
