bb3956bdb17536f5cd7ac111b44d1defe801f09a
Alin <alinenachescu033@gmail.com>
2017-09-23 14:16:27 +0300

    added test for logging_init #97

--- tests/src/logging/test_logging.c
+++ tests/src/logging/test_logging.c
@@ -2,9 +2,29 @@
 
 #include <cut.h>
 
+#include "kernel/include/logging.h"
+
+extern logging_writePfn g_pfn[];
+extern uint32 g_numOfPfn;
+extern bool g_isInitialized;
+
+PhiOS_STATIC_ASSERT(sizeof(bool) == sizeof(uint8), PhiOS_ASSERT_MESSAGE);
+
 CUT_DEFINE_TEST(test_logging_init)
 {
+    CUT_CHECK_OPERATOR_UINT8(g_isInitialized, ==, false);
 
+    CUT_CHECK_OPERATOR_UINT32(logging_init(), ==, ERROR_SUCCESS);
+
+    uint32 capacity = logging_getPfnArrayCapacity();
+    for (uint32 i = 0; i < capacity; i++) {
+        CUT_CHECK_OPERATOR_ADDRESS((void*)g_pfn[i], ==, (void*)NULL);
+    }
+
+    CUT_CHECK_OPERATOR_UINT32(g_numOfPfn, ==, 0);
+    CUT_CHECK_OPERATOR_UINT8(g_isInitialized, ==, true);
+
+    logging_uninit();
 }
 
 CUT_DEFINE_MAIN
