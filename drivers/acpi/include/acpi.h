b451c032fe6b0df4338dd70eeb50e2c1bce2ed79
Alin <alinenachescu033@gmail.com>
2017-09-28 21:36:18 +0300

    implemented acpi_getRSDP()

--- drivers/acpi/include/acpi.h
+++ drivers/acpi/include/acpi.h
@@ -34,6 +34,22 @@ struct _RSDP2
 typedef struct _RSDP2   RSDP2;
 typedef struct _RSDP2* PRSDP2;
 
+/*
+ * @brief: Get root system description pointer.
+ *
+ * @param:
+ *  rsdp2 - pointer to RSDP struct where the structure is moved if it's found.
+ *      The data in the structure is not modified if RSDP was not found.
+ *  acpiVersion - here it's stored the version of the RSDP.
+ *      ACPI_VERSION_UNKNOWN it's stored if RSDP is not found.
+ *
+ * @return:
+ *  ERROR_SUCCESS - RSDP was found.
+ *  ERROR_NULL_POINTER - rsdp2 or acpiVersion are null.
+ *  ERROR_NOT_FOUND - RSDP was not found.
+ *  ERROR_UNKNOWN_VERSION - unknown version of RSDP.
+ *  ERROR_INTERNAL_ERROR - something went wrong.
+ */
 uint32 acpi_getRSDP(
     PRSDP2 rsdp2,
     uint32 *acpiVersion
