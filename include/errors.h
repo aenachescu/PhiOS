a5720be9171bd6d7c863f5acb8787138cb2dc67b
Alin <alinenachescu033@gmail.com>
2017-09-28 23:26:29 +0300

    added ERROR_INCOMPATIBLE_VERSIONS and ERROR_INCOMPATIBLE

--- include/errors.h
+++ include/errors.h
@@ -27,6 +27,8 @@
 #define ERROR_NO_FREE_VIRTUAL_MEMORY 21
 #define ERROR_UNKNOWN_VERSION       22
 #define ERROR_INTERNAL_ERROR        23
+#define ERROR_INCOMPATIBLE_VERSIONS 24
+#define ERROR_INCOMPATIBLE          25
 
 void __kernel_check(size_t err, const char *file, size_t line);
 #define _kernel_check(err, file, line) __kernel_check(err, file, line)
