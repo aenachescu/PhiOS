5d2786e752ccfb7bfd6e0e82713f92ca7f0ce7dd
Alin <alinenachescu033@gmail.com>
2017-09-28 21:36:18 +0300

    implemented acpi_getRSDP()

--- drivers/acpi/src/acpi.c
+++ drivers/acpi/src/acpi.c
@@ -1,4 +1,6 @@
 #include "drivers/acpi/include/acpi.h"
+#include "kernel/include/logging.h"
+#include "util/kstdlib/include/kstring.h"
 
 uint32 acpi_getRSDP(
     PRSDP2 rsdp2,
@@ -13,5 +15,76 @@ uint32 acpi_getRSDP(
     }
 
     *acpiVersion = ACPI_VERSION_UNKNOWN;
-    return ERROR_NOT_FOUND;
+
+    /*
+     * TODO:
+     *  Checks if it's uefi or legacy system.
+     *      1. It's uefi system - get address from EFI_SYSTEM_TABLE.
+     *      2. It's legacy system:
+     *          2a. Search RSDP/RSDP2 in the memory area from 0x000E0000 to 0x000FFFFF
+     *          2b. Search RSDP/RSDP2 in the first 1KiB of the EBDA.
+     */
+
+    uint8 *ptr = (uint8*) 0x000E0000;
+    sint32 cmpRes = 0;
+    uint8 checksum = 0;
+
+    for (; ptr < (uint8*)0x00100000; ptr += 16) {
+        if (kmemcmp(ptr, "RSD PTR ", 8, &cmpRes) != ERROR_SUCCESS) {
+            continue;
+        }
+
+        if (cmpRes != 0) {
+            continue;
+        }
+
+        // checks checksum for RSDP2
+        checksum = 0;
+        for (size_t i = 0; i < sizeof(RSDP2); i++) {
+            checksum += ptr[i];
+        }
+
+        if (checksum == 0) {
+            *acpiVersion = ACPI_VERSION_2;
+            break;
+        }
+
+        // checks checksum for RSDP
+        checksum = 0;
+        for (size_t i = 0; i < sizeof(RSDP); i++) {
+            checksum += ptr[i];
+        }
+
+        if (checksum == 0) {
+            *acpiVersion = ACPI_VERSION_1;
+            break;
+        }
+    }
+
+    if (*acpiVersion == ACPI_VERSION_UNKNOWN) {
+        KLOG_INFO("Not found RSDP");
+        return ERROR_NOT_FOUND;
+    }
+
+    KLOG_INFO("Found RSDP at [%0P], version = %u", ptr, *acpiVersion);
+
+    switch (*acpiVersion) {
+        case ACPI_VERSION_1:
+            if (kmemcpy(rsdp2, ptr, sizeof(RSDP)) != ERROR_SUCCESS) {
+                return ERROR_INTERNAL_ERROR;
+            }
+            break;
+
+        case ACPI_VERSION_2:
+            if (kmemcpy(rsdp2, ptr, sizeof(RSDP2)) != ERROR_SUCCESS) {
+                return ERROR_INTERNAL_ERROR;
+            }
+            break;
+
+        default:
+            *acpiVersion = ACPI_VERSION_UNKNOWN;
+            return ERROR_UNKNOWN_VERSION;
+    }
+
+    return ERROR_SUCCESS;
 }
