e89ed9561da8fcbc1f1eccd6187fd80f3ad64e5a
Alin <alinenachescu033@gmail.com>
2017-10-04 22:45:50 +0300

    now, serial is initialized in init32.c

--- kernel/src/phi_main.c
+++ kernel/src/phi_main.c
@@ -13,7 +13,6 @@ extern struct Paging g_kernelPaging;
 
 #include "drivers/keyboard/include/keyboard.h"
 #include "drivers/rtc/include/rtc.h"
-#include "drivers/serial/include/serial.h"
 #include "drivers/video/include/vga/text_mode.h"
 
 #include "util/kstdlib/include/kstdio.h"
@@ -83,9 +82,6 @@ void kernel_main()
     // Inits keyboard
     KERNEL_CHECK(keyboard_init());
     kprintf("[SYSTEM] Initialized keyboard.\n");
-
-    serial_init();
-    serial_writeString("oh, serial is working :o\n", SERIAL_PORT_A);
 
     struct AllocFuncParam request;
     struct IA32_4KB_Paging_AllocParam ia32_request;
