97ca5ee78d0c8233a09f9a778062d2bb7769cc01
Alin <alinenachescu033@gmail.com>
2017-10-08 22:25:54 +0300

    created the SRAT structures

---
+++ drivers/acpi/include/acpi_srat.h
@@ -0,0 +1,103 @@
+#ifndef PhiOS_DRIVERS_ACPI_SRAT
+#define PhiOS_DRIVERS_ACPI_SRAT
+
+#include "include/types.h"
+#include "include/errors.h"
+#include "include/compiler.h"
+
+/*
+ *  SRAT header
+ */
+
+#define SRAT_PROCESSOR_LOCAL_APIC_TYPE      0
+#define SRAT_MEMORY_TYPE                    1
+#define SRAT_PROCESSOR_LOCAL_x2APIC_TYPE    2
+
+struct _SRAT
+{
+    char    signature[4];       // Contains "SRAT".
+    uint32  length;             // Length, in bytes, of the entire SRAT. The length implies the 
+                                // number of Entry fields at the end of the table
+    uint8   revision;           // 3
+    uint8   checksum;           // Entire table must sum to zero.
+    uint8   OEMID[6];           // OEM ID.
+    uint64  OEMTableID;         // For the SRAT, the table ID is the manufacturer model ID.
+    uint32  OEMRevision;        // OEM revision of System Resource Affinity Table for supplied OEM Table ID.
+    uint32  creatorID;          // Vendor ID of utility that created the table.
+    uint32  creatorRevision;    // Revision of utility that created the table.
+    uint8   reserved[12];       // Reserved.
+} PhiOS_PACKED_STRUCTURE;
+
+typedef struct _SRAT   SRAT;
+typedef struct _SRAT* PSRAT;
+
+/*
+ *  SRAT processor local apic structure
+ */
+
+#define PROCESSOR_LOCAL_APIC_FLAG_ENABLED 1
+
+struct _SRATProcessorLocalApic
+{
+    uint8   type;               // 0x0 for this type of structure.
+    uint8   length;             // 16
+    uint8   lowDomain;          // Bits [0:7] of the proximity domain.
+    uint8   apicId;             // The processor's local APIC ID.
+    uint32  flags;              // Flags.
+    uint8   sapicEId;           // The processor's local SAPIC EID.
+    uint8   highDomain[3];      // Bits [8:31] of the proximity domain.
+    uint32  clockDomain;        // The clock domain to which the processor belongs.
+} PhiOS_PACKED_STRUCTURE;
+
+typedef struct _SRATProcessorLocalApic   SRATProcessorLocalApic;
+typedef struct _SRATProcessorLocalApic* PSRATProcessorLocalApic;
+
+/*
+ *  SRAT memory structure
+ */
+
+#define MEMORY_FLAG_ENABLED         1
+#define MEMORY_FLAG_HOT_PLUGGABLE   2
+#define MEMORY_FLAG_NON_VOLATILE    4
+
+struct _SRATMemory
+{
+    uint8   type;           // 0x1 for this type of structure.
+    uint8   length;         // 40
+    uint32  domain;         // Integer that represents the proximity domain
+                            // to which the processor belongs.
+    uint8   reserved1[2];   // Reserved.
+    uint32  lowBase;        // Low 32 bits of the base address of the memory range.
+    uint32  highBase;       // High 32 bits of the base address of the memory range.
+    uint32  lowLength;      // Low 32 bits of the length of the range.
+    uint32  highLength;     // High 32 bits of the length of the range.
+    uint8   reserved2[4];   // Reserved.
+    uint32  flags;          // Flags.
+    uint8   reserved3[8];   // Reserved.
+} PhiOS_PACKED_STRUCTURE;
+
+typedef struct _SRATMemory   SRATMemory;
+typedef struct _SRATMemory* PSRATMemory;
+
+/*
+ *  SRAT processor local x2 apic structure
+ */
+
+#define PROCESSOR_LOCAL_x2APIC_FLAG_ENABLED 1
+
+struct _SRATProcessorLocalx2Apic
+{
+    uint8   type;           // 0x2 for this type of structure.
+    uint8   length;         // 24
+    uint8   reserved1[2];   // Reserved.
+    uint32  domain;         // The proximity domain to which the logical processor belongs.
+    uint32  x2apicId;       // The processor local x2APIC ID.
+    uint32  flags;          // Flags.
+    uint32  clockDomain;    // The clock domain to which the logical processor belongs.
+    uint8   reserved2[4];   // Reserved.
+} PhiOS_PACKED_STRUCTURE;
+
+typedef struct _SRATProcessorLocalx2Apic   SRATProcessorLocalx2Apic;
+typedef struct _SRATProcessorLocalx2Apic* PSRATProcessorLocalx2Apic;
+
+#endif
