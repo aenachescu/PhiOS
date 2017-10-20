a4ebfb8bc99a94b0981c0e27b08582a7ab1cd49f
Alin <alinenachescu033@gmail.com>
2017-10-20 23:50:14 +0300

    cleanup after build

--- linux_x86_64_compile.sh
+++ linux_x86_64_compile.sh
@@ -16,6 +16,7 @@
 
 # $2 can take the following values: x86_32, x86_64
 
+##### helper functions
 
 function remove_dir {
     if [ -d "$1" ]; then
@@ -30,6 +31,19 @@ function create_dir {
     cd $1
 }
 
+##### x86_32 build
+
+function clean_x86_32 {
+    if [ -f "../../kernel/src/arch/x86/linker32.ld" ]; then
+        rm "../../kernel/src/arch/x86/linker32.ld"
+    fi
+}
+
+function exit_x86_32 {
+    clean_x86_32
+    exit $1
+}
+
 function build_x86_32 {
     export PATH=$PWD/build/linux-x86_64/gcc-x86_32/bin:$PWD/build/linux-x86_64/cmake/bin:$PATH
     
@@ -42,14 +56,14 @@ function build_x86_32 {
     rc=$?
     if [[ $rc != 0 ]]; then
         echo "cmake failed - $rc"
-        exit $rc
+        exit_x86_32 $rc
     fi
 
     make
     rc=$?
     if [[ $rc != 0 ]]; then
         echo "make failed - $rc"
-        exit $rc
+        exit_x86_32 $rc
     fi
 
     if [ "$1" == "false" ]; then
@@ -58,9 +72,24 @@ function build_x86_32 {
         rc=$?
         if [[ $rc != 0 ]]; then
             echo "make iso failed - $rc"
-            exit $rc
+            exit_x86_32 $rc
         fi
-    fi    
+    fi
+
+    clean_x86_32
+}
+
+##### x86_64 build
+
+function clean_x86_64 {
+    if [ -f "../../kernel/src/arch/x86/linker64.ld" ]; then
+        rm "../../kernel/src/arch/x86/linker64.ld"
+    fi
+}
+
+function exit_x86_64 {
+    clean_x86_64
+    exit $1
 }
 
 function build_x86_64 {
@@ -75,14 +104,14 @@ function build_x86_64 {
     rc=$?
     if [[ $rc != 0 ]]; then
         echo "cmake failed - $rc"
-        exit $rc
+        exit_x86_64 $rc
     fi
 
     make
     rc=$?
     if [[ $rc != 0 ]]; then
         echo "make failed - $rc"
-        exit $rc
+        exit_x86_64 $rc
     fi
 
     if [ "$1" == "false" ]; then
@@ -91,16 +120,22 @@ function build_x86_64 {
         rc=$?
         if [[ $rc != 0 ]]; then
             echo "make iso failed - $rc"
-            exit $rc
+            exit_x86_64 $rc
         fi
     fi
+
+    clean_x86_64
 }
+
+
+##### main
 
 if [ $# -lt 2 ]; then
     echo "Illegal number of parameters"
     exit 1
 fi
 
+# build
 if [ "$1" == "build" ];then
 
     if [ $# -gt 3 ]; then
@@ -134,6 +169,7 @@ if [ "$1" == "build" ];then
     exit 1
 fi
 
+# clean
 if [ "$1" == "clean" ]; then
 
     if [ $# -ne 2 ]; then
@@ -156,6 +192,8 @@ if [ "$1" == "clean" ]; then
     echo "invalid parameter for clean command"
     exit 1
 fi
+
+# invalid syntax
 
 echo "invalid syntax"
 
