6bc23c88db205ba6df86e160490d55d4e3c2078d
Alin <alinenachescu033@gmail.com>
2017-10-26 21:38:43 +0300

    refactoring; implemented AVLStruct_init, AVLStruct_free, AVLNodeStruct_init, AVLNodeStruct_create functions

--- kernel/include/dataStructs/avl.h
+++ kernel/include/dataStructs/avl.h
@@ -5,64 +5,202 @@
 #include "include/errors.h"
 #include "include/preprocessor.h"
 
-typedef void* (*PfnAlloc)(size_t size);
+// uint32 copyType(type* dest, type* src, size_t size)
+#ifndef AVL_COPY_TYPE_FUNC
+#error "copy function is mandatory"
+#endif
 
-#ifdef AVL_USE_PFN_ALLOC
-
-#define AVL_PFN_ALLOC(x) PfnAlloc x;
-#define AVL_PFN_ALLOC_DECLARE_PARAM(x) , PfnAlloc x
-#define AVL_PFN_ALLOC_USE_PARAM(x) x
-#define AVL_SET_ALLOC_FN(avl, value) avl->allocFn = value
-#define AVL_CHECK_IF_ALLOC_FN_IS_NOT_NULL(allocFn) (allocFn != NULL)
-#define AVL_CHECK_IF_ALLOC_FN_IS_NULL(allocFn) (allocFn == NULL)
-
-#else
-
-#define AVL_PFN_ALLOC(x)
-#define AVL_PFN_ALLOC_DECLARE_PARAM(x)
-#define AVL_PFN_ALLOC_USE_PARAM(x)
-#define AVL_SET_ALLOC_FN(avl, allocFn)
-#define AVL_CHECK_IF_ALLOC_FN_IS_NOT_NULL(allocFn) (true)
-#define AVL_CHECK_IF_ALLOC_FN_IS_NULL(allocFn) (false)
-
+#ifndef AVL_FREE_TYPE_FUNC
+#define AVL_FREE_TYPE_FUNC(x)
 #endif
 
 #define AVLStruct(name) CONCAT(name, AVL)
 #define AVLFunc(name, func) CONCAT(AVLStruct(name), CONCAT(_, func))
 
+#define AVLNodeStruct(name) CONCAT(name, AVLNode)
+#define AVLNodeFunc(name, func) CONCAT(AVLNodeStruct(name), CONCAT(_, func))
+
 #define DECLARE_AVL_TYPE(type, name)                                            \
-typedef struct CONCAT(CONCAT(name, AVLNode), _t)                                \
-{                                                                               \
-    struct CONCAT(CONCAT(name, AVLNode), _t) *left;                             \
-    struct CONCAT(CONCAT(name, AVLNode), _t) *right;                            \
-    type data;                                                                  \
-} CONCAT(name, AVLNode);                                                        \
-                                                                                \
-typedef struct CONCAT(CONCAT(name, AVL), _t)                                    \
-{                                                                               \
-    CONCAT(name, AVLNode) *root;                                                \
-    AVL_PFN_ALLOC(allocFn)                                                      \
-} AVLStruct(name);                                                              \
-                                                                                \
-uint32 AVLFunc(name, init) (                                                    \
-    AVLStruct(name) *a_avl                                                      \
-    AVL_PFN_ALLOC_DECLARE_PARAM(a_allocFn)                                      \
-);
+    DECLARE_AVL_NODE_STRUCT(type, name)                                         \
+    DECLARE_AVL_STRUCT(type, name)                                              \
+    DECLARE_AVL_FUNC_INIT(type, name)                                           \
+    DECLARE_AVL_FUNC_FREE(type, name)                                           \
+    DECLARE_AVL_NODE_FUNC_INIT(type, name)                                      \
+    DECLARE_AVL_NODE_FUNC_CREATE(type, name)
 
 #define IMPLEMENT_AVL_TYPE(type, name)                                          \
+    IMPLEMENT_AVL_FUNC_INIT(type, name)                                         \
+    IMPLEMENT_AVL_FUNC_FREE(type, name)                                         \
+    IMPLEMENT_AVL_NODE_FUNC_INIT(type, name)                                    \
+    IMPLEMENT_AVL_NODE_FUNC_CREATE(type, name)
+
+// declare AVLNodeStruct
+#define DECLARE_AVL_NODE_STRUCT(type, name)                                     \
+typedef struct CONCAT(AVLNodeStruct(name), _t)                                  \
+{                                                                               \
+    struct CONCAT(AVLNodeStruct(name), _t) *left;                               \
+    struct CONCAT(AVLNodeStruct(name), _t) *right;                              \
+    uint32 height;                                                              \
+    type data;                                                                  \
+} AVLNodeStruct(name);                                                          \
+
+// declare AVLStruct
+#define DECLARE_AVL_STRUCT(type, name)                                          \
+typedef struct CONCAT(AVLStruct(name), _t)                                      \
+{                                                                               \
+    AVLNodeStruct(name) *root;                                                  \
+} AVLStruct(name);                                                              \
+
+// declare & implement INIT function for avl node
+#define DECLARE_AVL_NODE_FUNC_INIT(type, name)                                  \
+uint32 AVLNodeFunc(name, init) (                                                \
+    AVLNodeStruct(name) *a_node,                                                \
+    type *a_data                                                                \
+);
+
+#define IMPLEMENT_AVL_NODE_FUNC_INIT(type, name)                                \
+uint32 AVLNodeFunc(name, init) (                                                \
+    AVLNodeStruct(name) *a_node,                                                \
+    type *a_data)                                                               \
+{                                                                               \
+    if (a_node == NULL) {                                                       \
+        return ERROR_NULL_POINTER;                                              \
+    }                                                                           \
+                                                                                \
+    if (a_data == NULL) {                                                       \
+        return ERROR_NULL_POINTER;                                              \
+    }                                                                           \
+                                                                                \
+    a_node->left = NULL;                                                        \
+    a_node->right = NULL;                                                       \
+    a_node->height = 1;                                                         \
+                                                                                \
+    uint32 err = AVL_COPY_TYPE_FUNC(&a_node->data, a_data, sizeof(type));       \
+    if (err != ERROR_SUCCESS) {                                                 \
+        return err;                                                             \
+    }                                                                           \
+                                                                                \
+    return ERROR_SUCCESS;                                                       \
+}
+
+// if AVL_ALLOC_FUNC is defined the declare & implement CREATE function
+// for avl node
+#ifdef AVL_ALLOC_FUNC
+
+#define DECLARE_AVL_NODE_FUNC_CREATE(type, name)                                \
+uint32 AVLNodeFunc(name, create) (                                              \
+    AVLNodeStruct(name) **a_node,                                               \
+    type *a_data                                                                \
+);
+
+#define IMPLEMENT_AVL_NODE_FUNC_CREATE(type, name)                              \
+uint32 AVLNodeFunc(name, create) (                                              \
+    AVLNodeStruct(name) **a_node,                                               \
+    type *a_data)                                                               \
+{                                                                               \
+    if (a_node == NULL) {                                                       \
+        return ERROR_NULL_POINTER;                                              \
+    }                                                                           \
+                                                                                \
+    if (a_data == NULL) {                                                       \
+        return ERROR_NULL_POINTER;                                              \
+    }                                                                           \
+                                                                                \
+    *a_node = (AVLNodeStruct(name)*) AVL_ALLOC_FUNC(sizeof(*a_node));           \
+    if (a_node == NULL) {                                                       \
+        return ERROR_NO_FREE_MEMORY;                                            \
+    }                                                                           \
+                                                                                \
+    (*a_node)->left = NULL;                                                     \
+    (*a_node)->right = NULL;                                                    \
+    (*a_node)->height = 1;                                                      \
+                                                                                \
+    uint32 err = AVL_COPY_TYPE_FUNC(&(*a_node)->data, a_data, sizeof(type));    \
+    if (err != ERROR_SUCCESS) {                                                 \
+        return err;                                                             \
+    }                                                                           \
+                                                                                \
+    return ERROR_SUCCESS;                                                       \
+}
+
+#else
+
+#define DECLARE_AVL_NODE_FUNC_CREATE(type, name)
+#define IMPLEMENT_AVL_NODE_FUNC_CREATE(type, name)
+
+#endif
+
+// declare & implement INIT function
+#define DECLARE_AVL_FUNC_INIT(type, name)                                       \
 uint32 AVLFunc(name, init) (                                                    \
     AVLStruct(name) *a_avl                                                      \
-    AVL_PFN_ALLOC_DECLARE_PARAM(a_allocFn))                                     \
+);
+
+#define IMPLEMENT_AVL_FUNC_INIT(type, name)                                     \
+uint32 AVLFunc(name, init) (                                                    \
+    AVLStruct(name) *a_avl)                                                     \
 {                                                                               \
-    if (a_avl != NULL &&                                                        \
-        AVL_CHECK_IF_ALLOC_FN_IS_NULL(a_allocFn)) {                             \
+    if (a_avl == NULL) {                                                        \
         return ERROR_NULL_POINTER;                                              \
     }                                                                           \
                                                                                 \
     a_avl->root = NULL;                                                         \
-    AVL_SET_ALLOC_FN(a_avl, a_allocFn);                                         \
                                                                                 \
     return ERROR_SUCCESS;                                                       \
 }
+
+// if AVL_FREE_FUNC is defined then declare & implement FREE function
+#ifdef AVL_FREE_FUNC
+
+#define DECLARE_AVL_FUNC_FREE(type, name)                                       \
+uint32 AVLNodeFunc(name, free) (                                                \
+    AVLNodeStruct(name) *a_node                                                 \
+);                                                                              \
+                                                                                \
+uint32 AVLFunc(name, free) (                                                    \
+    AVLStruct(name) *a_avl                                                      \
+);
+
+#define IMPLEMENT_AVL_FUNC_FREE(type, name)                                     \
+uint32 AVLNodeFunc(name, free) (                                                \
+    AVLNodeStruct(name) *a_node)                                                \
+{                                                                               \
+    if (a_node == NULL) {                                                       \
+        return ERROR_NULL_POINTER;                                              \
+    }                                                                           \
+                                                                                \
+    AVLNodeFunc(name, free)(a_node->left);                                      \
+    AVLNodeFunc(name, free)(a_node->right);                                     \
+                                                                                \
+    AVL_FREE_TYPE_FUNC(a_node->data);                                           \
+                                                                                \
+    AVL_FREE_FUNC(a_node);                                                      \
+                                                                                \
+    return ERROR_SUCCESS;                                                       \
+}                                                                               \
+                                                                                \
+uint32 AVLFunc(name, free) (                                                    \
+    AVLStruct(name) *a_avl)                                                     \
+{                                                                               \
+    if (a_avl == NULL) {                                                        \
+        return ERROR_NULL_POINTER;                                              \
+    }                                                                           \
+                                                                                \
+    if (a_avl->root == NULL) {                                                  \
+        return ERROR_UNINITIALIZED;                                             \
+    }                                                                           \
+                                                                                \
+    AVLNodeFunc(name, free)(a_avl->root);                                       \
+    a_avl->root = NULL;                                                         \
+                                                                                \
+    return ERROR_SUCCESS;                                                       \
+}
+
+#else
+
+#define DECLARE_AVL_FUNC_FREE(type, name)
+#define IMPLEMENT_AVL_FUNC_FREE(type, name)
+
+#endif
 
 #endif
