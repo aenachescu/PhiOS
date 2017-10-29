a603b888ec54afb36a2d92d7aa8174845c570e7e
Alin <alinenachescu033@gmail.com>
2017-10-29 15:12:02 +0200

    added AVL_ERROR_NO_FREE_MEMORY and AVL_ERROR_INTERNAL_ERROR

--- kernel/include/dataStructs/avl/avl_errors.h
+++ kernel/include/dataStructs/avl/avl_errors.h
@@ -6,5 +6,7 @@ typedef unsigned int avl_error_code_t;
 #define AVL_ERROR_SUCCESS           ((avl_error_code_t) 0)
 #define AVL_ERROR_NULL_POINTER      ((avl_error_code_t) 1)
 #define AVL_ERROR_NOT_FOUND         ((avl_error_code_t) 2)
+#define AVL_ERROR_NO_FREE_MEMORY    ((avl_error_code_t) 3)
+#define AVL_ERROR_INTERNAL_ERROR    ((avl_error_code_t) 4)
 
 #endif // end if CLIB_TEMPLATE_DATA_STRUCTURES_AVL_ERRORS
