cb2a94aa0c3e32456937f15c9841204e5535d18c
Alin <alinenachescu033@gmail.com>
2017-10-29 20:55:57 +0200

    added avlnode_uninit()

---
+++ kernel/include/dataStructs/avl/avlnode_uninit.h
@@ -0,0 +1,31 @@
+#ifndef CLIB_TEMPLATE_DATA_STRUCTURES_AVL_NODE_UNINIT
+#define CLIB_TEMPLATE_DATA_STRUCTURES_AVL_NODE_UNINIT
+
+// void avlDestroyType(type *a_ptr)
+#ifndef AVL_DESTROY_TYPE_FUNC
+#define AVL_DESTROY_TYPE_FUNC(data)
+#endif
+
+#define DECLARE_AVL_NODE_FUNC_UNINIT(type, name)                                \
+avl_error_code_t AVLNodeFunc(name, uninit) (                                    \
+    AVLNodeStruct(name) *a_node                                                 \
+);
+
+#define IMPLEMENT_AVL_NODE_FUNC_UNINIT(type, name)                              \
+avl_error_code_t AVLNodeFunc(name, uninit) (                                    \
+    AVLNodeStruct(name) *a_node)                                                \
+{                                                                               \
+    if (a_node == CLIB_NULLPTR) {                                               \
+        return AVL_ERROR_NULL_POINTER;                                          \
+    }                                                                           \
+                                                                                \
+    a_node->left = CLIB_NULLPTR;                                                \
+    a_node->right = CLIB_NULLPTR;                                               \
+    a_node->height = 0;                                                         \
+                                                                                \
+    AVL_DESTROY_TYPE_FUNC((&a_node->data));                                     \
+                                                                                \
+    return AVL_ERROR_SUCCESS;                                                   \
+}
+
+#endif // end if CLIB_TEMPLATE_DATA_STRUCTURES_AVL_NODE_UNINIT
