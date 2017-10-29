30c64f71a260293c492ef82a01d94a7646f1386c
Alin <alinenachescu033@gmail.com>
2017-10-29 11:09:52 +0200

    the result is stored in a_res parameter and the functions returns an error code;

--- kernel/include/dataStructs/avl/avl_getHeight.h
+++ kernel/include/dataStructs/avl/avl_getHeight.h
@@ -3,18 +3,26 @@
 
 #define DECLARE_AVL_FUNC_GET_HEIGHT(type, name)                                 \
 uint32 AVLFunc(name, getHeight) (                                               \
-    const AVLStruct(name) *a_avl                                                \
+    const AVLStruct(name) *a_avl,                                               \
+    uint32 *a_res                                                               \
 );
 
 #define IMPLEMENT_AVL_FUNC_GET_HEIGHT(type, name)                               \
 uint32 AVLFunc(name, getHeight) (                                               \
-    const AVLStruct(name) *a_avl)                                               \
+    const AVLStruct(name) *a_avl,                                               \
+    uint32 *a_res)                                                              \
 {                                                                               \
     if (a_avl == NULL) {                                                        \
-        return 0;                                                               \
+        return ERROR_NULL_POINTER;                                              \
     }                                                                           \
                                                                                 \
-    return AVLNodeFunc(name, getHeight) (a_avl->root);                          \
+    if (a_res == NULL) {                                                        \
+        return ERROR_NULL_POINTER;                                              \
+    }                                                                           \
+                                                                                \
+    *a_res = AVLNodeFunc(name, getHeight) (a_avl->root);                        \
+                                                                                \
+    return ERROR_SUCCESS;                                                       \
 }
 
 #endif // end if PhiOS_DATA_STRUCTURES_AVL_GET_HEIGHT
