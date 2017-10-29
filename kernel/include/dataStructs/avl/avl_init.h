43cebb1716d05b1b87a296bb4416e314a8f77eb8
Alin <alinenachescu033@gmail.com>
2017-10-29 14:08:24 +0200

    used avl_error_code_t; now avl is independent of OS and can be moved into a separate project;

--- kernel/include/dataStructs/avl/avl_init.h
+++ kernel/include/dataStructs/avl/avl_init.h
@@ -2,21 +2,21 @@
 #define CLIB_TEMPLATE_DATA_STRUCTURES_AVL_INIT
 
 #define DECLARE_AVL_FUNC_INIT(type, name)                                       \
-uint32 AVLFunc(name, init) (                                                    \
+avl_error_code_t AVLFunc(name, init) (                                          \
     AVLStruct(name) *a_avl                                                      \
 );
 
 #define IMPLEMENT_AVL_FUNC_INIT(type, name)                                     \
-uint32 AVLFunc(name, init) (                                                    \
+avl_error_code_t AVLFunc(name, init) (                                          \
     AVLStruct(name) *a_avl)                                                     \
 {                                                                               \
     if (a_avl == NULL) {                                                        \
-        return ERROR_NULL_POINTER;                                              \
+        return AVL_ERROR_NULL_POINTER;                                          \
     }                                                                           \
                                                                                 \
     a_avl->root = NULL;                                                         \
                                                                                 \
-    return ERROR_SUCCESS;                                                       \
+    return AVL_ERROR_SUCCESS;                                                   \
 }
 
 #endif // end if CLIB_TEMPLATE_DATA_STRUCTURES_AVL_INIT
--- kernel/include/dataStructs/avl/avlnode_init.h
+++ kernel/include/dataStructs/avl/avlnode_init.h
@@ -1,40 +1,40 @@
 #ifndef CLIB_TEMPLATE_DATA_STRUCTURES_AVL_NODE_INIT
 #define CLIB_TEMPLATE_DATA_STRUCTURES_AVL_NODE_INIT
 
-// uint32 copyType(type *a_dest, const type *a_src)
+// avl_error_code_t copyType(type *a_dest, const type *a_src)
 #ifndef AVL_COPY_TYPE_FUNC
 #error "copy function is mandatory"
 #endif
 
 #define DECLARE_AVL_NODE_FUNC_INIT(type, name)                                  \
-uint32 AVLNodeFunc(name, init) (                                                \
+avl_error_code_t AVLNodeFunc(name, init) (                                      \
     AVLNodeStruct(name) *a_node,                                                \
     const type *a_data                                                          \
 );
 
 #define IMPLEMENT_AVL_NODE_FUNC_INIT(type, name)                                \
-uint32 AVLNodeFunc(name, init) (                                                \
+avl_error_code_t AVLNodeFunc(name, init) (                                      \
     AVLNodeStruct(name) *a_node,                                                \
     const type *a_data)                                                         \
 {                                                                               \
     if (a_node == NULL) {                                                       \
-        return ERROR_NULL_POINTER;                                              \
+        return AVL_ERROR_NULL_POINTER;                                          \
     }                                                                           \
                                                                                 \
     if (a_data == NULL) {                                                       \
-        return ERROR_NULL_POINTER;                                              \
+        return AVL_ERROR_NULL_POINTER;                                          \
     }                                                                           \
                                                                                 \
     a_node->left = NULL;                                                        \
     a_node->right = NULL;                                                       \
     a_node->height = 1;                                                         \
                                                                                 \
-    uint32 err = AVL_COPY_TYPE_FUNC((&a_node->data), a_data);                   \
-    if (err != ERROR_SUCCESS) {                                                 \
+    avl_error_code_t err = AVL_COPY_TYPE_FUNC((&a_node->data), a_data);         \
+    if (err != AVL_ERROR_SUCCESS) {                                             \
         return err;                                                             \
     }                                                                           \
                                                                                 \
-    return ERROR_SUCCESS;                                                       \
+    return AVL_ERROR_SUCCESS;                                                   \
 }
 
 #endif // end if CLIB_TEMPLATE_DATA_STRUCTURES_AVL_NODE_INIT
