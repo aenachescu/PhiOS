97e2baa1d1308495fa47986d16a544386b815d85
Alin <alinenachescu033@gmail.com>
2017-10-29 17:51:09 +0200

    added tests for init and free

--- tests/src/avl/test_avl.c
+++ tests/src/avl/test_avl.c
@@ -2,11 +2,56 @@
 
 #include "tests/src/avl/avl_impl.h"
 
+#define RESET_STATISTICS    \
+    ResetMemoryInUsage();   \
+    ResetObjectsInUsage();  \
+    ResetAllocCalls();      \
+    ResetFreeCalls();
+
+#define CHECK_STATISTICS                                                \
+    CUT_CHECK_OPERATOR_SIZE_T(GetMemoryInUsage(), ==, 0);               \
+    CUT_CHECK_OPERATOR_SIZE_T(GetObjectsInUsage(), ==, 0);              \
+    CUT_CHECK_OPERATOR_SIZE_T(GetAllocCalls(), ==, GetFreeCalls());
+
 CUT_DEFINE_TEST(test_avl_init)
 {
-    CUT_CHECK_OPERATOR_UINT32(0, ==, 0);
+    RESET_STATISTICS;
+
+    UTDataAVL tree;
+
+    CUT_CHECK(UTDataAVL_init(NULL) == AVL_ERROR_NULL_POINTER);
+    CUT_CHECK(UTDataAVL_init(&tree) == AVL_ERROR_SUCCESS);
+
+    UTDataAVL_free(&tree);
+
+    CHECK_STATISTICS;
+}
+
+CUT_DEFINE_TEST(test_avl_free)
+{
+    RESET_STATISTICS;
+
+    UTDataAVL tree;
+    Data data;
+
+    CUT_CHECK(UTDataAVL_init(&tree) == AVL_ERROR_SUCCESS);
+
+    CUT_CHECK(UTDataAVL_free(NULL) == AVL_ERROR_NULL_POINTER);
+    CUT_CHECK(UTDataAVL_free(&tree) == AVL_ERROR_SUCCESS);
+
+    CUT_CHECK(UTDataAVL_init(&tree) == AVL_ERROR_SUCCESS);
+
+    for (int i = 0; i < 10; i ++) {
+        data.data = i;
+        CUT_CHECK(UTDataAVL_insert(&tree, &data) == AVL_ERROR_SUCCESS);
+    }
+
+    CUT_CHECK(UTDataAVL_free(&tree) == AVL_ERROR_SUCCESS);
+
+    CHECK_STATISTICS;
 }
 
 CUT_DEFINE_MAIN
     CUT_CALL_TEST(test_avl_init);
+    CUT_CALL_TEST(test_avl_free);
 CUT_END_MAIN
