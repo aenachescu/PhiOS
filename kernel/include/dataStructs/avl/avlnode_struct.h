a3c037f910b57223acb66c9aed3cd1af4be8e81a
Alin <alinenachescu033@gmail.com>
2017-10-29 13:43:10 +0200

    defined its own preprocessor.h and error codes;

--- kernel/include/dataStructs/avl/avlnode_struct.h
+++ kernel/include/dataStructs/avl/avlnode_struct.h
@@ -1,14 +1,32 @@
 #ifndef PhiOS_DATA_STRUCTURES_AVL_NODE_STRUCT
 #define PhiOS_DATA_STRUCTURES_AVL_NODE_STRUCT
 
-#define AVLNodeStruct(name) CONCAT(name, AVLNode)
-#define AVLNodeFunc(name, func) CONCAT(AVLNodeStruct(name), CONCAT(_, func))
+#define AVLNodeStruct(name)                                                     \
+    C_TEMPLATE_DATA_STRUCTURES_CONCAT(                                          \
+        name,                                                                   \
+        AVLNode                                                                 \
+    )
+
+#define AVLNodeStruct_t(name)                                                   \
+    C_TEMPLATE_DATA_STRUCTURES_CONCAT(                                          \
+        AVLNodeStruct(name),                                                    \
+        _t                                                                      \
+    )
+
+#define AVLNodeFunc(name, func)                                                 \
+    C_TEMPLATE_DATA_STRUCTURES_CONCAT(                                          \
+        AVLNodeStruct(name),                                                    \
+        C_TEMPLATE_DATA_STRUCTURES_CONCAT(                                      \
+            _,                                                                  \
+            func                                                                \
+        )                                                                       \
+    )
 
 #define DECLARE_AVL_NODE_STRUCT(type, name)                                     \
-typedef struct CONCAT(AVLNodeStruct(name), _t)                                  \
+typedef struct AVLNodeStruct_t(name)                                            \
 {                                                                               \
-    struct CONCAT(AVLNodeStruct(name), _t) *left;                               \
-    struct CONCAT(AVLNodeStruct(name), _t) *right;                              \
+    struct AVLNodeStruct_t(name) *left;                                         \
+    struct AVLNodeStruct_t(name) *right;                                        \
     uint32 height;                                                              \
     type data;                                                                  \
 } AVLNodeStruct(name);
